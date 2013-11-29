46c17bb3e924073e79b3834a59d83724d2721216
Matt Jibson <matt.jibson@gmail.com>
2013-11-28 23:11:16 -0500

    Lexer tests

--- conf/lex_test.go
+++ conf/lex_test.go
@@ -1,17 +1,62 @@
 package conf
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestLex(t *testing.T) {
-	input := `[testeoo] ntoh = eo02oen`
-	l := lex("test", input)
-	for i := range l.items {
-		fmt.Println("item", i)
-		if i.typ == itemEOF || i.typ == itemError {
-			break
+	type lexTest struct {
+		input string
+		valid bool
+	}
+	tests := []lexTest{
+		{
+			`[testeoo] ntoh = eo02oen`,
+			true,
+		},
+		{
+			`etnohu=2323eee oenuhoaetnsu oentu
+[testeoo]
+ntoh = eo02oen
+oenoet=2309,.0uh  o0.,
+[ok]
+yessir=,.,o ehountes`,
+			true,
+		},
+		{
+			`yeasnot`,
+			false,
+		},
+		{
+			`[aoenth`,
+			false,
+		},
+		{
+			`aoenth=`,
+			true,
+		},
+		{
+			`oaesunth=aoesunth`,
+			true,
+		},
+	}
+
+Loop:
+	for _, test := range tests {
+		l := lex("test", test.input)
+		for i := range l.items {
+			if i.typ == itemEOF {
+				break
+			} else if i.typ == itemError {
+				if test.valid {
+					t.Fatal("Produced error on valid input:", test.input)
+				} else {
+					continue Loop
+				}
+			}
+		}
+		if !test.valid {
+			t.Fatal("Expected an error:", test.input)
 		}
 	}
 }
