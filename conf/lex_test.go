3cc7f0b881cdf171f3209c32626e269e3b250882
Matt Jibson <matt.jibson@gmail.com>
2013-11-30 01:40:16 -0500

    New test structure

--- conf/lex_test.go
+++ conf/lex_test.go
@@ -1,62 +1,52 @@
 package conf
 
 import (
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
 	"testing"
 )
 
-func TestLex(t *testing.T) {
-	type lexTest struct {
-		input string
-		valid bool
+func isValid(fname string, t *testing.T) bool {
+	t.Log(fname)
+	b, err := ioutil.ReadFile(fname)
+	if err != nil {
+		return false
 	}
-	tests := []lexTest{
-		{
-			`[testeoo] ntoh = eo02oen`,
-			true,
-		},
-		{
-			`etnohu=2323eee oenuhoaetnsu oentu
-[testeoo]
-ntoh = eo02oen
-oenoet=2309,.0uh  o0.,
-[ok]
-yessir=,.,o ehountes`,
-			true,
-		},
-		{
-			`yeasnot`,
-			false,
-		},
-		{
-			`[aoenth`,
-			false,
-		},
-		{
-			`aoenth=`,
-			true,
-		},
-		{
-			`oaesunth=aoesunth`,
-			true,
-		},
-	}
-
-Loop:
-	for _, test := range tests {
-		l := lex("test", test.input)
-		for i := range l.items {
-			if i.typ == itemEOF {
-				break
-			} else if i.typ == itemError {
-				if test.valid {
-					t.Fatal("Produced error on valid input:", test.input)
-				} else {
-					continue Loop
-				}
-			}
+	l := lex(fname, string(b))
+	for i := range l.items {
+		t.Log("item", i)
+		if i.typ == itemEOF {
+			return true
+		} else if i.typ == itemError {
+			return false
 		}
-		if !test.valid {
-			t.Fatal("Expected an error:", test.input)
+	}
+	return false
+}
+
+func testDir(dirname string, valid bool, t *testing.T) {
+	files, _ := ioutil.ReadDir(dirname)
+	for _, f := range files {
+		p := filepath.Join(dirname, f.Name())
+		if isValid(p, t) != valid {
+			t.Fatalf("%v: expected %v", p, valid)
+		}
+	}
+}
+
+func TestLex(t *testing.T) {
+	testDir("test_valid", true, t)
+	testDir("test_invalid", false, t)
+}
+
+func TestPrint(t *testing.T) {
+	input := `test = hi`
+	l := lex("test", input)
+	for i := range l.items {
+		fmt.Println("item", i)
+		if i.typ == itemEOF || i.typ == itemError {
+			break
 		}
 	}
 }
