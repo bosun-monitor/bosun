b1d985f8b7eb68a2fc32db9e4ae3cf05ad66995b
Matt Jibson <matt.jibson@gmail.com>
2013-12-03 14:47:31 -0500

    cmd/scollector: Not how this works: no cookie for @kylebrandt

--- cmd/scollector/collectors/echo_crap.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-while :; do
-	#if it is in the spirit of tcollector, the hostname should be added by the main loop
-	echo "put crap $(date +%s) $(( ( RANDOM % 10 )  + 1 ))"
-	sleep 15
-done
-	
