9776c93526a2c82569d176b9bcba574be339f497
Kyle Brandt <kyle@kbrandt.com>
2013-12-11 06:42:40 -0500

    cmd/scollector: Finish converting disk_windows to CreateQuery

--- cmd/scollector/collectors/disk_windows.go
+++ cmd/scollector/collectors/disk_windows.go
@@ -6,28 +6,9 @@ import (
 )
 
 func init() {
-	//collectors = append(collectors, Collector{c_physical_disk_windows, DEFAULT_FREQ_SEC})
+	collectors = append(collectors, Collector{F: c_physical_disk_windows})
 	collectors = append(collectors, Collector{F: c_diskspace_windows})
 }
-
-const PHYSICAL_DISK_QUERY = `
-	SELECT Name, AvgDisksecPerRead, AvgDisksecPerWrite,
-		AvgDiskReadQueueLength, AvgDiskWriteQueueLength, 
-		DiskReadBytesPersec, DiskReadsPersec,
-		DiskWriteBytesPersec, DiskWritesPersec, 
-		SplitIOPerSec, PercentDiskReadTime, PercentDiskWriteTime
-	FROM Win32_PerfRawData_PerfDisk_PhysicalDisk
-	WHERE Name <> '_Total'
-`
-
-// Similar breakdowns exist as to physical, but for now just using this for the
-// space utilization.
-
-const DISKSPACE_QUERY = `
-	SELECT Name, FreeMegabytes, PercentFreeSpace
-	FROM Win32_PerfRawData_PerfDisk_LogicalDisk
-	WHERE Name <> '_Total'
-`
 
 func c_diskspace_windows() opentsdb.MultiDataPoint {
 	var dst []Win32_PerfRawData_PerfDisk_LogicalDisk
@@ -39,21 +20,22 @@ func c_diskspace_windows() opentsdb.MultiDataPoint {
 	}
 	var md opentsdb.MultiDataPoint
 	for _, v := range dst {
-		//Add(&md, "disk.logical.free_bytes", v.FreeMegabytes*1048576, opentsdb.TagSet{"partition": v.Name})
+		Add(&md, "disk.logical.free_bytes", v.FreeMegabytes*1048576, opentsdb.TagSet{"partition": v.Name})
 		Add(&md, "disk.logical.percent_free", v.PercentFreeSpace, opentsdb.TagSet{"partition": v.Name})
 	}
 	return md
 }
 
 type Win32_PerfRawData_PerfDisk_LogicalDisk struct {
-	//FreeMegabytes    uint32
+	FreeMegabytes    uint32
 	Name             string
 	PercentFreeSpace uint32
 }
 
 func c_physical_disk_windows() opentsdb.MultiDataPoint {
 	var dst []Win32_PerfRawData_PerfDisk_PhysicalDisk
-	err := wmi.Query(PHYSICAL_DISK_QUERY, &dst)
+	var q = wmi.CreateQuery(&dst, `WHERE Name <> '_Total'`)
+	err := wmi.Query(q, &dst)
 	if err != nil {
 		l.Println("disk_physical:", err)
 		return nil
