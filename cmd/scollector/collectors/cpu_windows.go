896ed9752fa1a40d0b9c26a790bda44e5e5ff09c
Kyle Brandt <kyle@kbrandt.com>
2013-12-12 13:28:35 -0500

    cmd/scollector: Add some more WMI CPU information

--- cmd/scollector/collectors/cpu_windows.go
+++ cmd/scollector/collectors/cpu_windows.go
@@ -22,13 +22,26 @@ func c_cpu_windows() opentsdb.MultiDataPoint {
 		Add(&md, "cpu.time", v.PercentPrivilegedTime, opentsdb.TagSet{"cpu": v.Name, "type": "privileged"})
 		Add(&md, "cpu.time", v.PercentInterruptTime, opentsdb.TagSet{"cpu": v.Name, "type": "interrupt"})
 		Add(&md, "cpu.time", v.PercentUserTime, opentsdb.TagSet{"cpu": v.Name, "type": "user"})
+		Add(&md, "cpu.time_idle", v.PercentIdleTime, opentsdb.TagSet{"cpu": v.Name})
+		Add(&md, "cpu.interrupts", v.InterruptsPersec, opentsdb.TagSet{"cpu": v.Name})
+		Add(&md, "cpu.dpcs", v.InterruptsPersec, opentsdb.TagSet{"cpu": v.Name})
+		Add(&md, "cpu.time_cstate", v.PercentC1Time, opentsdb.TagSet{"cpu": v.Name, "type": "c1"})
+		Add(&md, "cpu.time_cstate", v.PercentC2Time, opentsdb.TagSet{"cpu": v.Name, "type": "c2"})
+		Add(&md, "cpu.time_cstate", v.PercentC3Time, opentsdb.TagSet{"cpu": v.Name, "type": "c3"})
 	}
 	return md
 }
 
 type Win32_PerfRawData_PerfOS_Processor struct {
 	Name                  string
+	PercentC1Time         uint64
+	PercentC2Time         uint64
+	PercentC3Time         uint64
+	InterruptsPersec      uint32
+	DPCRate               uint32
 	PercentInterruptTime  uint64
 	PercentPrivilegedTime uint64
 	PercentUserTime       uint64
+	PercentProcessorTime  uint64
+	PercentIdleTime       uint64
 }
