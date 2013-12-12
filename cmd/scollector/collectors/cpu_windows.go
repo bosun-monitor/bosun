633168b88fca8b9d6afdfada365cb5510829be0a
Kyle Brandt <kyle@kbrandt.com>
2013-12-12 16:28:25 -0500

    cmd/scollector: Yet more WMI cpu info

--- cmd/scollector/collectors/cpu_windows.go
+++ cmd/scollector/collectors/cpu_windows.go
@@ -7,6 +7,7 @@ import (
 
 func init() {
 	collectors = append(collectors, Collector{F: c_cpu_windows})
+	collectors = append(collectors, Collector{F: c_cpu_info_windows})
 }
 
 func c_cpu_windows() opentsdb.MultiDataPoint {
@@ -44,4 +45,35 @@ type Win32_PerfRawData_PerfOS_Processor struct {
 	PercentUserTime       uint64
 	PercentProcessorTime  uint64
 	PercentIdleTime       uint64
+}
+
+func c_cpu_info_windows() opentsdb.MultiDataPoint {
+	var dst []Win32_Processor
+	var q = wmi.CreateQuery(&dst, `WHERE Name <> '_Total'`)
+	err := wmi.Query(q, &dst)
+	if err != nil {
+		l.Println("cpu_info:", err)
+		return nil
+	}
+	var md opentsdb.MultiDataPoint
+	for _, v := range dst {
+		Add(&md, "cpu.clock", v.CurrentClockSpeed, opentsdb.TagSet{"cpu": v.Name})
+		Add(&md, "cpu.clock_max", v.MaxClockSpeed, opentsdb.TagSet{"cpu": v.Name})
+		Add(&md, "cpu.voltage", v.CurrentVoltage, opentsdb.TagSet{"cpu": v.Name})
+		Add(&md, "cpu.load", v.LoadPercentage, opentsdb.TagSet{"cpu": v.Name})
+		Add(&md, "cpu.cores_physical", v.NumberOfCores, opentsdb.TagSet{"cpu": v.Name})
+		Add(&md, "cpu.cores_logical", v.NumberOfLogicalProcessors, opentsdb.TagSet{"cpu": v.Name})
+	}
+	return md
+}
+
+//This is actually a CIM_Processor according to C# reflection
+type Win32_Processor struct {
+	CurrentClockSpeed         uint32
+	CurrentVoltage            uint16
+	LoadPercentage            uint16
+	MaxClockSpeed             uint32
+	Name                      string
+	NumberOfCores             uint32
+	NumberOfLogicalProcessors uint32
 }
