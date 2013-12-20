4e60b5278075c9eef00ba680190eee98fa879c5b
Matt Jibson <matt.jibson@gmail.com>
2013-12-20 16:42:01 -0500

    Use a modified text/template/parse/lex_test.go
    Much more test coverage, and found a few bugs the previous tests didn't.

--- expr/lex_test.go
+++ expr/lex_test.go
@@ -1,70 +1,186 @@
+// Copyright 2011 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
 
 package expr
-import "testing"
 
-type lexTest struct {
-	s string
-	v bool
+import (
+	"fmt"
+	"testing"
+)
+
+// Make the types prettyprint.
+var itemName = map[itemType]string{
+	itemError:      "error",
+	itemEOF:        "EOF",
+	itemNot:        "!",
+	itemAnd:        "&&",
+	itemOr:         "||",
+	itemGreater:    ">",
+	itemLess:       "<",
+	itemGreaterEq:  ">=",
+	itemLessEq:     "<=",
+	itemEq:         "==",
+	itemNotEq:      "!=",
+	itemPlus:       "+",
+	itemMinus:      "-",
+	itemMult:       "*",
+	itemDiv:        "/",
+	itemNumber:     "number",
+	itemComma:      ",",
+	itemLeftParen:  "(",
+	itemRightParen: ")",
+	itemString:     "string",
+	itemQuery:      "query",
+	itemFunc:       "func",
 }
 
-func isValid(s string) bool {
-	l := lex("", s)
-	for i := range l.items {
-		if i.typ == itemEOF {
-			return true
-		} else if i.typ == itemError {
+func (i itemType) String() string {
+	s := itemName[i]
+	if s == "" {
+		return fmt.Sprintf("item%d", int(i))
+	}
+	return s
+}
+
+type lexTest struct {
+	name  string
+	input string
+	items []item
+}
+
+var (
+	tEOF   = item{itemEOF, 0, ""}
+	tLpar  = item{itemLeftParen, 0, "("}
+	tRpar  = item{itemRightParen, 0, ")"}
+	tComma = item{itemComma, 0, ","}
+	tLt    = item{itemLess, 0, "<"}
+	tGt    = item{itemGreater, 0, ">"}
+	tOr    = item{itemOr, 0, "||"}
+)
+
+var lexTests = []lexTest{
+	{"empty", "", []item{tEOF}},
+	{"spaces", " \t\n", []item{tEOF}},
+	{"text", `"now is the time"`, []item{{itemString, 0, `"now is the time"`}, tEOF}},
+	{"numbers", "1 02 0x14 7.2 1e3 1.2e-4", []item{
+		{itemNumber, 0, "1"},
+		{itemNumber, 0, "02"},
+		{itemNumber, 0, "0x14"},
+		{itemNumber, 0, "7.2"},
+		{itemNumber, 0, "1e3"},
+		{itemNumber, 0, "1.2e-4"},
+		tEOF,
+	}},
+	{"expression", `avg([m=sum:sys.cpu.user{host=*-web*}], "1m") < 0.2 || avg([m=sum:sys.cpu.user{host=*-web*}], "1m") > 0.4`, []item{
+		{itemFunc, 0, "avg"},
+		tLpar,
+		{itemQuery, 0, "[m=sum:sys.cpu.user{host=*-web*}]"},
+		tComma,
+		{itemString, 0, `"1m"`},
+		tRpar,
+		tLt,
+		{itemNumber, 0, "0.2"},
+		tOr,
+		{itemFunc, 0, "avg"},
+		tLpar,
+		{itemQuery, 0, "[m=sum:sys.cpu.user{host=*-web*}]"},
+		tComma,
+		{itemString, 0, `"1m"`},
+		tRpar,
+		tGt,
+		{itemNumber, 0, "0.4"},
+		tEOF,
+	}},
+	// errors
+	{"unclosed quote", "\"", []item{
+		{itemError, 0, "unterminated string"},
+	}},
+	{"unclosed query", "[m=sum:m", []item{
+		{itemError, 0, "unterminated query"},
+	}},
+}
+
+// collect gathers the emitted items into a slice.
+func collect(t *lexTest) (items []item) {
+	l := lex(t.name, t.input)
+	for {
+		item := l.nextItem()
+		items = append(items, item)
+		if item.typ == itemEOF || item.typ == itemError {
+			break
+		}
+	}
+	return
+}
+
+func equal(i1, i2 []item, checkPos bool) bool {
+	if len(i1) != len(i2) {
+		return false
+	}
+	for k := range i1 {
+		if i1[k].typ != i2[k].typ {
+			return false
+		}
+		if i1[k].val != i2[k].val {
+			return false
+		}
+		if checkPos && i1[k].pos != i2[k].pos {
 			return false
 		}
 	}
-	return false
+	return true
 }
 
 func TestLex(t *testing.T) {
-	type lexTest struct {
-		s string
-		v bool
-	}
-	valids := []string{
-		"1",
-		"1 > 0",
-		"2 + 3",
-		"3 - 4",
-		"4 * 5",
-		"5 / 6",
-		"!10",
-		"7 && 8",
-		"9 || 10",
-		"11 < 12",
-		"13 > 14",
-		"15 == 16",
-		"17 != 18",
-		"19 <= 20",
-		"21 >= 21",
-		"+1",
-		"-1",
-		".0",
-		"-.7e-9",
-		"+1 + +1",
-		"-1 - -1",
-		"0x1",
-		`avg([m=sum:sys.cpu.user{host=*}], "1m") < 0.7`,
-		`avg([m=sum:sys.cpu.user{host=*-web*}], "1m") < 0.2 || avg([m=sum:sys.cpu.user{host=*-web*}], "1m") > 0.4`,
-	}
-	invalids := []string{
-		"1 -+ 2",
-		"avg(1 | 3)",
-	}
-	var lexTests []lexTest
-	for _, v := range valids {
-		lexTests = append(lexTests, lexTest{v, true})
-	}
-	for _, v := range invalids {
-		lexTests = append(lexTests, lexTest{v, false})
-	}
 	for _, test := range lexTests {
-		v := isValid(test.s)
-		if v != test.v {
-			t.Errorf("expected %v: %v", test.v, test.s)
+		items := collect(&test)
+		if !equal(items, test.items, false) {
+			t.Errorf("%s: got\n\t%+v\nexpected\n\t%v", test.name, items, test.items)
 		}
 	}
 }
+
+/*
+var lexPosTests = []lexTest{
+	{"empty", "", []item{tEOF}},
+	{"punctuation", "{{,@%#}}", []item{
+		{itemLeftDelim, 0, "{{"},
+		{itemChar, 2, ","},
+		{itemChar, 3, "@"},
+		{itemChar, 4, "%"},
+		{itemChar, 5, "#"},
+		{itemRightDelim, 6, "}}"},
+		{itemEOF, 8, ""},
+	}},
+	{"sample", "0123{{hello}}xyz", []item{
+		{itemText, 0, "0123"},
+		{itemLeftDelim, 4, "{{"},
+		{itemIdentifier, 6, "hello"},
+		{itemRightDelim, 11, "}}"},
+		{itemText, 13, "xyz"},
+		{itemEOF, 16, ""},
+	}},
+}
+
+// The other tests don't check position, to make the test cases easier to construct.
+// This one does.
+func TestPos(t *testing.T) {
+	for _, test := range lexPosTests {
+		items := collect(&test, "", "")
+		if !equal(items, test.items, true) {
+			t.Errorf("%s: got\n\t%v\nexpected\n\t%v", test.name, items, test.items)
+			if len(items) == len(test.items) {
+				// Detailed print; avoid item.String() to expose the position value.
+				for i := range items {
+					if !equal(items[i:i+1], test.items[i:i+1], true) {
+						i1 := items[i]
+						i2 := test.items[i]
+						t.Errorf("\t#%d: got {%v %d %q} expected  {%v %d %q}", i, i1.typ, i1.pos, i1.val, i2.typ, i2.pos, i2.val)
+					}
+				}
+			}
+		}
+	}
+}
+*/
