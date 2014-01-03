688cdde743a131b95e8903af75570d461abe7aa0
Matt Jibson <matt.jibson@gmail.com>
2014-01-02 19:11:04 -0500

    cmd/scollector: Fix clean test

--- cmd/scollector/opentsdb/tsdb_test.go
+++ cmd/scollector/opentsdb/tsdb_test.go
@@ -4,12 +4,18 @@ import "testing"
 
 func TestClean(t *testing.T) {
 	clean := "aoeSNVT152-./_"
-	if Clean(clean) != clean {
+	if c, err := Clean(clean); c != clean {
 		t.Error("was clean", clean)
+	} else if err != nil {
+		t.Fatal(err)
 	}
 	notclean := "euon@#$sar:.03   n  ]e/"
 	notcleaned := "euonsar.03ne/"
-	if c := Clean(notclean); c != notcleaned {
+	if c, err := Clean(notclean); c != notcleaned {
 		t.Error("wasn't cleaned", notclean, "into:", c)
+	} else if err != nil {
+		t.Fatal(err)
+	}
+}
 	}
 }
