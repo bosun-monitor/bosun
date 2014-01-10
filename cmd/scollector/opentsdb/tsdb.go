f827835d9ecc596e3f2f8a66532f42309c88006b
Matt Jibson <matt.jibson@gmail.com>
2014-01-10 15:16:27 -0500

    cmd/scollector: Return an err on bad status
    Previously was returning nil.

--- cmd/scollector/opentsdb/tsdb.go
+++ cmd/scollector/opentsdb/tsdb.go
@@ -294,8 +294,13 @@ func (r Request) Query(host string) (ResponseSet, error) {
 		return nil, err
 	}
 	resp, err := http.Post(u.String(), "application/json", bytes.NewReader(b))
-	if err != nil || resp.StatusCode != http.StatusOK {
+	defer resp.Body.Close()
+	if err != nil {
 		return nil, err
+	} else if resp.StatusCode != http.StatusOK {
+		b, err = ioutil.ReadAll(resp.Body)
+		fmt.Println(string(b))
+		return nil, fmt.Errorf("tsdb: %s", resp.Status)
 	}
 	b, err = ioutil.ReadAll(resp.Body)
 	if err != nil {
