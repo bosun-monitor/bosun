f3cb745b34ecea18abcd6f37acdce7db95650224
Matt Jibson <matt.jibson@gmail.com>
2014-01-30 17:14:18 -0500

    Fix up test

--- conf/conf_test.go
+++ conf/conf_test.go
@@ -1,22 +1,18 @@
 package conf
 
 import (
-	"fmt"
 	"io/ioutil"
 	"testing"
 )
 
 func TestPrint(t *testing.T) {
-	fname := "parse/test_valid/4"
-	fname = "../dev.conf"
+	fname := "../dev.conf"
 	b, err := ioutil.ReadFile(fname)
 	if err != nil {
 		t.Fatal(err)
 	}
-	c, err := New(fname, string(b))
+	_, err = New(fname, string(b))
 	if err != nil {
 		t.Error(err)
-	} else {
-		fmt.Println(c)
 	}
 }
