8b399445132908d998033bbb2303676e9232ea6a
Kyle Brandt <kyle@kbrandt.com>
2014-01-31 14:03:28 -0500

    cmd/scollector: os namespace for windows cpu

--- cmd/scollector/collectors/cpu_windows.go
+++ cmd/scollector/collectors/cpu_windows.go
@@ -20,7 +20,13 @@ func c_cpu_windows() opentsdb.MultiDataPoint {
 		return nil
 	}
 	var md opentsdb.MultiDataPoint
+	var total_percent uint64 = 0
+	var core_count uint64 = 0
+	var ts uint64 = 0
 	for _, v := range dst {
+		total_percent += v.PercentIdleTime
+		core_count += 1
+		ts = v.Timestamp_Sys100NS
 		Add(&md, "win.cpu", v.PercentPrivilegedTime, opentsdb.TagSet{"cpu": v.Name, "type": "privileged"})
 		Add(&md, "win.cpu", v.PercentInterruptTime, opentsdb.TagSet{"cpu": v.Name, "type": "interrupt"})
 		Add(&md, "win.cpu", v.PercentUserTime, opentsdb.TagSet{"cpu": v.Name, "type": "user"})
@@ -31,6 +37,9 @@ func c_cpu_windows() opentsdb.MultiDataPoint {
 		Add(&md, "win.cpu.time_cstate", v.PercentC2Time, opentsdb.TagSet{"cpu": v.Name, "type": "c2"})
 		Add(&md, "win.cpu.time_cstate", v.PercentC3Time, opentsdb.TagSet{"cpu": v.Name, "type": "c3"})
 	}
+	if core_count != 0 {
+		Add(&md, "os.cpu", ((ts / 1e5) - (total_percent/core_count)/1e5), nil)
+	}
 	return md
 }
 
@@ -39,6 +48,7 @@ type Win32_PerfRawData_PerfOS_Processor struct {
 	InterruptsPersec      uint32
 	Name                  string
 	PercentC1Time         uint64
+	Timestamp_Sys100NS    uint64
 	PercentC2Time         uint64
 	PercentC3Time         uint64
 	PercentIdleTime       uint64
