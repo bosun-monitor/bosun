43abbd711cf05f3d4a3e46071694d83c3ff03a25
Kyle Brandt <kyle@kbrandt.com>
2014-02-03 10:49:55 -0500

    Attempt to replicate the items page in the new angular/type script version. Works, but ts error with lacking call signatures.

--- web/static/js/tsaf.js
+++ web/static/js/tsaf.js
@@ -10,6 +10,9 @@ tsafApp.config([
         $routeProvider.when('/', {
             templateUrl: 'partials/dashboard.html',
             controller: 'DashboardCtrl'
+        }).when('/items', {
+            templateUrl: 'partials/items.html',
+            controller: 'ItemsCtrl'
         }).otherwise({
             redirectTo: '/'
         });
@@ -29,8 +32,56 @@ var Schedule = (function () {
     return Schedule;
 })();
 
+var Metric = (function () {
+    function Metric() {
+    }
+    return Metric;
+})();
+
+var Hosts = (function () {
+    function Hosts() {
+    }
+    return Hosts;
+})();
+
 tsafControllers.controller('DashboardCtrl', [
     '$scope', '$http', function ($scope, $http) {
         $http.get('/api/alerts').success(function (data) {
         });
+    }]);
+
+tsafControllers.controller('ItemsCtrl', [
+    '$scope', 'TmService', function ($scope, TmService) {
+        $scope.metrics;
+        $scope.hosts;
+
+        getMetrics();
+        getHosts();
+
+        function getMetrics() {
+            TmService.getMetrics().success(function (metrics) {
+                $scope.metrics = metrics;
+            }).error(function (error) {
+                $scope.status = 'Unable to fetch metrics: ' + error.message;
+            });
+        }
+
+        function getHosts() {
+            TmService.getHosts().success(function (hosts) {
+                $scope.hosts = hosts;
+            }).error(function (error) {
+                $scope.status = 'Unable to fetch hosts: ' + error.message;
+            });
+        }
+    }]);
+
+tsafApp.service('TmService', [
+    '$http', function ($http) {
+        this.getMetrics = function () {
+            return $http.get('/api/metric');
+        };
+
+        this.getHosts = function () {
+            return $http.get('/api/tagv/host');
+        };
     }]);
--- web/static/js/tsaf.ts
+++ web/static/js/tsaf.ts
@@ -12,6 +12,10 @@ tsafApp.config(['$routeProvider', function($routeProvider: ng.route.IRouteProvid
 			templateUrl: 'partials/dashboard.html',
 			controller: 'DashboardCtrl',
 		}).
+		when('/items', {
+			templateUrl: 'partials/items.html',
+			controller: 'ItemsCtrl',
+		}).
 		otherwise({
 			redirectTo: '/',
 		});
@@ -20,18 +24,76 @@ tsafApp.config(['$routeProvider', function($routeProvider: ng.route.IRouteProvid
 var tsafControllers = angular.module('tsafControllers', []);
 
 class Alert {
-	
 }
 
 class Schedule {
 }
 
-interface IDashboardScope extends ng.IScope {
+class Metric {
+}
+
+class Hosts {
+}
+
+interface IDashboardScope extends ng.IScope {Í
 	alerts: Alert[];
+}
+
+interface IItemsScope extends ng.IScope {Í
+	metrics: Metric[];
+	hosts: Hosts[];
+	status: string;
+}
+
+interface ITmService extends ng.IHttpService {
+	getMetrics: Metric[];
+	getHosts: Hosts[];
 }
 
 tsafControllers.controller('DashboardCtrl', ['$scope', '$http', function($scope: IDashboardScope, $http: ng.IHttpService) {
 	$http.get('/api/alerts').success(function(data) {
 		
 	});
+}]);
+
+tsafControllers.controller('ItemsCtrl', ['$scope', 'TmService', function($scope: IItemsScope, TmService: ITmService){
+	
+	$scope.metrics;
+	$scope.hosts;
+
+	getMetrics();
+	getHosts();
+
+	function getMetrics() {
+		TmService.getMetrics()
+			.success(function (metrics: Metric[]) {
+				$scope.metrics = metrics;
+			})
+			.error(function (error) {
+				$scope.status = 'Unable to fetch metrics: ' + error.message;
+			});
+	}
+
+	function getHosts() {
+		TmService.getHosts()
+			.success(function (hosts: Hosts[]) {
+				$scope.hosts = hosts;
+			})
+			.error(function (error) {
+				$scope.status = 'Unable to fetch hosts: ' + error.message;
+			});
+	}
+
+}]);
+
+tsafApp.service('TmService', ['$http', function($http: ng.IHttpService) {
+
+	this.getMetrics = function () {
+		return $http.get('/api/metric');
+	};
+
+	this.getHosts = function () {
+		return $http.get('/api/tagv/host');
+	};
+
 }]);
