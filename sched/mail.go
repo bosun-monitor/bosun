ac5e0df8a527192df8d258b3d91d4adf5d5597d4
Matt Jibson <matt.jibson@gmail.com>
2014-02-05 20:28:08 -0500

    Roll our own SendMail function to fix TLS cert issues
    We have a signed cert, but it's not getting authorized, I think because we
    don't have the full chain installed. TSAF will be generally used by
    internal organizations, so now we can be lenient here on the cert
    verification.

--- sched/mail.go
+++ sched/mail.go
@@ -2,7 +2,11 @@ package sched
 
 import (
 	"bytes"
+	"crypto/tls"
+	"errors"
 	"log"
+	"net/mail"
+	"net/smtp"
 	"strings"
 
 	"github.com/StackExchange/scollector/opentsdb"
@@ -46,7 +50,7 @@ func (s *Schedule) Email(name string, group opentsdb.TagSet) {
 	e.To = strings.Split(a.Owner, ",")
 	e.Subject = subject.String()
 	e.Text = body.Bytes()
-	err := e.Send(s.SmtpHost, nil)
+	err := Send(e, s.SmtpHost)
 	if err != nil {
 		log.Println(err)
 		return
@@ -54,4 +58,66 @@ func (s *Schedule) Email(name string, group opentsdb.TagSet) {
 	ak := AlertKey{a.Name, group.String()}
 	state := s.Status[ak]
 	state.Emailed = true
+}
+
+// Send an email using the given host and SMTP auth (optional), returns any error thrown by smtp.SendMail
+// This function merges the To, Cc, and Bcc fields and calls the smtp.SendMail function using the Email.Bytes() output as the message
+func Send(e *email.Email, addr string) error {
+	// Merge the To, Cc, and Bcc fields
+	to := make([]string, 0, len(e.To)+len(e.Cc)+len(e.Bcc))
+	to = append(append(append(to, e.To...), e.Cc...), e.Bcc...)
+	// Check to make sure there is at least one recipient and one "From" address
+	if e.From == "" || len(to) == 0 {
+		return errors.New("Must specify at least one From address and one To address")
+	}
+	from, err := mail.ParseAddress(e.From)
+	if err != nil {
+		return err
+	}
+	raw, err := e.Bytes()
+	if err != nil {
+		return err
+	}
+	return SendMail(addr, from.Address, to, raw)
+}
+
+// SendMail connects to the server at addr, switches to TLS if
+// possible, authenticates with the optional mechanism a if possible,
+// and then sends an email from address from, to addresses to, with
+// message msg.
+func SendMail(addr string, from string, to []string, msg []byte) error {
+	c, err := smtp.Dial(addr)
+	if err != nil {
+		return err
+	}
+	defer c.Close()
+	if err = c.Hello("localhost"); err != nil {
+		return err
+	}
+	if ok, _ := c.Extension("STARTTLS"); ok {
+		if err = c.StartTLS(&tls.Config{InsecureSkipVerify: true}); err != nil {
+			return err
+		}
+	}
+	if err = c.Mail(from); err != nil {
+		return err
+	}
+	for _, addr := range to {
+		if err = c.Rcpt(addr); err != nil {
+			return err
+		}
+	}
+	w, err := c.Data()
+	if err != nil {
+		return err
+	}
+	_, err = w.Write(msg)
+	if err != nil {
+		return err
+	}
+	err = w.Close()
+	if err != nil {
+		return err
+	}
+	return c.Quit()
 }
