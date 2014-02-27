0a2db4063dd2c4f168278871f7cfa52e4539f9d7
Kyle Brandt <kyle@kbrandt.com>
2014-02-27 11:11:16 -0500

    bytes filter to make byte number human readable (GB, TB, etc)

--- web/static/js/tsaf.ts
+++ web/static/js/tsaf.ts
@@ -550,3 +550,14 @@ tsafApp.directive("tooltip", function() {
 		},
 	};
 });
+
+tsafApp.filter('bytes', function() {
+	return function(bytes: any, precision: number) {
+		if (bytes === 0) { return '0 bytes' };
+		if (isNaN(parseFloat(bytes)) || !isFinite(bytes)) return '-';
+		if (typeof precision === 'undefined') precision = 1;
+		var units = ['bytes', 'kB', 'MB', 'GB', 'TB', 'PB'],
+			number = Math.floor(Math.log(bytes) / Math.log(1024));
+		return (bytes / Math.pow(1024, Math.floor(number))).toFixed(precision) +  ' ' + units[number];
+	}
+});
