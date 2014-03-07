58bee206de371ef01a7b8bd728f74525960c5188
Kyle Brandt <kyle@kbrandt.com>
2014-03-07 11:08:54 -0500

    Template filter to convert a number to human readable bytes

--- sched/template.go
+++ sched/template.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"net/url"
 	"os"
+	"strconv"
 	"strings"
 
 	"github.com/StackExchange/tsaf/conf"
@@ -58,6 +59,53 @@ func (c *context) HostView(host string) string {
 		u.Host = h + u.Host
 	}
 	return u.String()
+}
+
+// Format a number (as string) into human readable bytes
+type ByteSize float64
+
+const (
+	_           = iota // ignore first value by assigning to blank identifier
+	KB ByteSize = 1 << (10 * iota)
+	MB
+	GB
+	TB
+	PB
+	EB
+	ZB
+	YB
+)
+
+func (b ByteSize) String() string {
+	switch {
+	case b >= YB:
+		return fmt.Sprintf("%.2fYB", b/YB)
+	case b >= ZB:
+		return fmt.Sprintf("%.2fZB", b/ZB)
+	case b >= EB:
+		return fmt.Sprintf("%.2fEB", b/EB)
+	case b >= PB:
+		return fmt.Sprintf("%.2fPB", b/PB)
+	case b >= TB:
+		return fmt.Sprintf("%.2fTB", b/TB)
+	case b >= GB:
+		return fmt.Sprintf("%.2fGB", b/GB)
+	case b >= MB:
+		return fmt.Sprintf("%.2fMB", b/MB)
+	case b >= KB:
+		return fmt.Sprintf("%.2fKB", b/KB)
+	}
+	return fmt.Sprintf("%.2fB", b)
+}
+
+func (c *context) HumanBytes(v string) (s string) {
+	f, err := strconv.ParseFloat(v, 64)
+	if err != nil {
+		return
+	}
+	b := ByteSize(f)
+	s = b.String()
+	return
 }
 
 func (s *Schedule) ExecuteBody(w io.Writer, a *conf.Alert, st *State) error {
