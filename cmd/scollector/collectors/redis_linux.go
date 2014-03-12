fbd79248026fee33da3d473b242c16006a12f944
Kyle Brandt <kyle@kbrandt.com>
2014-03-12 12:38:48 -0500

    cmd/scollector: Collect more Redis info

--- cmd/scollector/collectors/redis_linux.go
+++ cmd/scollector/collectors/redis_linux.go
@@ -18,30 +18,60 @@ func init() {
 }
 
 var FIELDS_REDIS = map[string]bool{
-	"bgrewriteaof_in_progress":   true,
-	"bgsave_in_progress":         true,
-	"blocked_clients":            true,
-	"changes_since_last_save":    true,
-	"client_biggest_input_buf":   true,
-	"client_longest_output_list": true,
-	"connected_clients":          true,
-	"connected_slaves":           true,
-	"evicted_keys":               true,
-	"expired_keys":               true,
-	"hash_max_zipmap_entries":    true,
-	"hash_max_zipmap_value":      true,
-	"keyspace_hits":              true,
-	"keyspace_misses":            true,
-	"mem_fragmentation_ratio":    true,
-	"pubsub_channels":            true,
-	"pubsub_patterns":            true,
-	"total_commands_processed":   true,
-	"total_connections_received": true,
-	"uptime_in_seconds":          true,
-	"used_cpu_sys":               true,
-	"used_cpu_user":              true,
-	"used_memory":                true,
-	"used_memory_rss":            true,
+	"aof_enabled":                  true,
+	"aof_rewrite_in_progress":      true,
+	"aof_rewrite_scheduled":        true,
+	"aof_last_rewrite_time_sec":    true,
+	"aof_current_rewrite_time_sec": true,
+	"aof_last_bgrewrite_status":    true,
+	"bgrewriteaof_in_progress":     true,
+	"bgsave_in_progress":           true,
+	"blocked_clients":              true,
+	"changes_since_last_save":      true,
+	"client_biggest_input_buf":     true,
+	"client_longest_output_list":   true,
+	"connected_clients":            true,
+	"connected_slaves":             true,
+	"evicted_keys":                 true,
+	"expired_keys":                 true,
+	"hash_max_zipmap_entries":      true,
+	"hash_max_zipmap_value":        true,
+	"keyspace_hits":                true,
+	"keyspace_misses":              true,
+	"master_link_status":           true,
+	"master_sync_in_progress":      true,
+	"master_last_io_seconds_ago":   true,
+	"master_sync_left_bytes":       true,
+	"mem_fragmentation_ratio":      true,
+	"pubsub_channels":              true,
+	"pubsub_patterns":              true,
+	"rdb_changes_since_last_save":  true,
+	"rdb_bgsave_in_progress":       true,
+	"rdb_last_save_time":           true,
+	"rdb_last_bgsave_status":       true,
+	"rdb_last_bgsave_time_sec":     true,
+	"rdb_current_bgsave_time_sec":  true,
+	"total_commands_processed":     true,
+	"total_connections_received":   true,
+	"uptime_in_seconds":            true,
+	"used_cpu_sys":                 true,
+	"used_cpu_user":                true,
+	"used_memory":                  true,
+	"used_memory_rss":              true,
+}
+
+//For master_link_status
+var MLS_MAP = map[string]string{
+	"up":   "1",
+	"down": "0",
+}
+
+//For aof_last_bgrewrite_status, rdb_last_bgsave_status
+func status(s string) string {
+	if s == "ok" {
+		return "1"
+	}
+	return "0"
 }
 
 var (
@@ -132,6 +162,14 @@ func c_redis_linux() opentsdb.MultiDataPoint {
 			line = strings.TrimSpace(line)
 			sp := strings.Split(line, ":")
 			if len(sp) < 2 || !FIELDS_REDIS[sp[0]] {
+				continue
+			}
+			if sp[0] == "master_link_status" {
+				Add(&md, "redis."+sp[0], MLS_MAP[sp[1]], tags)
+				continue
+			}
+			if sp[0] == "aof_last_bgrewrite_status" || sp[0] == "rdb_last_bgsave_status" {
+				Add(&md, "redis."+sp[0], status(sp[1]), tags)
 				continue
 			}
 			Add(&md, "redis."+sp[0], sp[1], tags)
