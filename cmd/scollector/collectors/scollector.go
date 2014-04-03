d1a742e0ed3e0e7e303b8dace6dcfa94ce36dff0
Matt Jibson <matt.jibson@gmail.com>
2014-04-03 16:08:50 -0400

    cmd/scollector: Log scollector stats every 30s

--- cmd/scollector/collectors/scollector.go
+++ cmd/scollector/collectors/scollector.go
@@ -3,12 +3,21 @@ package collectors
 import (
 	"runtime"
 	"sync"
+	"time"
 
 	"github.com/StackExchange/scollector/opentsdb"
+	"github.com/StackExchange/slog"
 )
 
 func init() {
 	collectors = append(collectors, &IntervalCollector{F: c_scollector})
+	go func() {
+		for _ = range time.Tick(time.Second * 30) {
+			for _, d := range c_scollector() {
+				slog.Info(d.Telnet())
+			}
+		}
+	}()
 }
 
 var (
