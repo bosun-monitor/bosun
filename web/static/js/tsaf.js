58df130e85c765b50f7de816c610bb2780763c3d
Matt Jibson <matt.jibson@gmail.com>
2014-04-15 20:14:39 -0400

    Default autods to true

--- web/static/js/tsaf.js
+++ web/static/js/tsaf.js
@@ -302,11 +302,8 @@ tsafControllers.controller('GraphCtrl', [
         $scope.query_p = request.queries;
         $scope.start = request.start;
         $scope.end = request.end;
-        $scope.autods = search.autods == 'true';
+        $scope.autods = search.autods != 'false';
         $scope.refresh = search.refresh == 'true';
-        if (typeof $scope.autods == 'undefined') {
-            $scope.autods = true;
-        }
         $scope.AddTab = function () {
             $scope.index = $scope.query_p.length;
             $scope.query_p.push(new Query);
@@ -388,7 +385,7 @@ tsafControllers.controller('GraphCtrl', [
             var r = getRequest();
             r.prune();
             $location.search('b64', btoa(JSON.stringify(r)));
-            $location.search('autods', $scope.autods ? 'true' : undefined);
+            $location.search('autods', $scope.autods ? undefined : 'false');
             $location.search('refresh', $scope.refresh ? 'true' : undefined);
             $route.reload();
         };
