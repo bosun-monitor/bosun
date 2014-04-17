d0cb08975178bb91e1b67812768de2c2c3928a30
Matt Jibson <matt.jibson@gmail.com>
2014-04-17 15:01:41 -0400

    party

--- sched/check.go
+++ sched/check.go
@@ -5,9 +5,9 @@ import (
 	"log"
 	"time"
 
-	"github.com/StackExchange/scollector/opentsdb"
 	"github.com/StackExchange/tsaf/conf"
 	"github.com/StackExchange/tsaf/expr"
+	"github.com/StackExchange/tsaf/third_party/github.com/StackExchange/scollector/opentsdb"
 )
 
 func (s *Schedule) Check() {
