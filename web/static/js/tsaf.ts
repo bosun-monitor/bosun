59b1c5f9dcea21714d734b1d9c0e74e567fdca57
Matt Jibson <matt.jibson@gmail.com>
2014-04-28 15:33:39 -0400

    Clear rateOptions when possible

--- web/static/js/tsaf.ts
+++ web/static/js/tsaf.ts
@@ -305,19 +305,20 @@ class Query {
 		}
 	}
 	setDerivative() {
+		var max = this.rateOptions.counterMax;
+		this.rateOptions = new RateOptions();
 		switch (this.derivative) {
 		case "rate":
 			this.rate = true;
-			this.rateOptions.counter = false;
 			break;
 		case "counter":
 			this.rate = true;
 			this.rateOptions.counter = true;
+			this.rateOptions.counterMax = max;
 			this.rateOptions.resetValue = 1;
 			break;
 		case "":
 			this.rate = false;
-			this.rateOptions.counter = false;
 			break;
 		}
 	}
