fee71bc1edff3db53e18dd020c8593cf025e3083
Kyle Brandt <kyle@kbrandt.com>
2014-05-06 15:06:18 -0500

    cmd/scollector: Don't send puppet enabled metric unless Puppet seems to be installed
    Also, use constants

--- cmd/scollector/collectors/puppet_linux.go
+++ cmd/scollector/collectors/puppet_linux.go
@@ -2,19 +2,41 @@ package collectors
 
 import (
 	"os"
+	"time"
 
 	"github.com/StackExchange/scollector/opentsdb"
 )
 
 func init() {
-	collectors = append(collectors, &IntervalCollector{F: puppet_disabled_func_linux})
+	collectors = append(collectors, &IntervalCollector{F: puppet_disabled_func_linux, init: puppetInit})
+}
+
+var puppetExists bool
+
+const puppetPath = "/var/lib/puppet/"
+const puppetDisabled = "/var/lib/puppet/state/agent_disabled.lock"
+
+func puppetInit() {
+	update := func() {
+		if _, err := os.Stat(puppetPath); err == nil {
+			puppetExists = true
+		}
+	}
+	update()
+	go func() {
+		for _ = range time.Tick(time.Minute * 5) {
+			update()
+		}
+	}()
 }
 
 func puppet_disabled_func_linux() opentsdb.MultiDataPoint {
+	if !puppetExists {
+		return nil
+	}
 	var md opentsdb.MultiDataPoint
 	disabled := 0
-	filename := "/var/lib/puppet/state/agent_disabled.lock"
-	if _, err := os.Stat(filename); !os.IsNotExist(err) {
+	if _, err := os.Stat(puppetDisabled); !os.IsNotExist(err) {
 		disabled = 1
 	}
 	Add(&md, "puppet.disabled", disabled, nil)
