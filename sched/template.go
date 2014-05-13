d9d9fc45679639e8eb869942e75f76d5eb4defb8
Kyle Brandt <kyle@kbrandt.com>
2014-05-13 14:48:34 -0500

    Export the context and some of it's fields

--- sched/template.go
+++ sched/template.go
@@ -14,18 +14,18 @@ import (
 	"github.com/StackExchange/tsaf/expr"
 )
 
-type context struct {
+type Context struct {
 	*State
 	Alert *conf.Alert
 
-	schedule *Schedule
+	Schedule *Schedule
 }
 
-func (s *Schedule) data(st *State, a *conf.Alert) *context {
-	return &context{
+func (s *Schedule) data(st *State, a *conf.Alert) *Context {
+	return &Context{
 		State:    st,
 		Alert:    a,
-		schedule: s,
+		Schedule: s,
 	}
 }
 
@@ -64,29 +64,29 @@ func (s *Schedule) URL() *url.URL {
 }
 
 // Ack returns the URL to acknowledge an alert.
-func (c *context) Ack() string {
-	u := c.schedule.URL()
+func (c *Context) Ack() string {
+	u := c.Schedule.URL()
 	u.Path = fmt.Sprintf("/api/acknowledge/%s/%s", c.Alert.Name, c.State.Group.String())
 	return u.String()
 }
 
 // HostView returns the URL to the host view page.
-func (c *context) HostView(host string) string {
-	u := c.schedule.URL()
+func (c *Context) HostView(host string) string {
+	u := c.Schedule.URL()
 	u.Path = "/host"
 	u.RawQuery = fmt.Sprintf("time=1d-ago&host=%s", host)
 	return u.String()
 }
 
-func (c *context) EGraph(v string) string {
+func (c *Context) EGraph(v string) string {
 	q := url.QueryEscape("q=" + opentsdb.ReplaceTags(v, c.Group))
 	u := url.URL{
 		Scheme:   "http",
-		Host:     c.schedule.Conf.HttpListen,
+		Host:     c.Schedule.Conf.HttpListen,
 		Path:     "/egraph",
 		RawQuery: q,
 	}
-	if strings.HasPrefix(c.schedule.Conf.HttpListen, ":") {
+	if strings.HasPrefix(c.Schedule.Conf.HttpListen, ":") {
 		h, err := os.Hostname()
 		if err != nil {
 			return ""
@@ -113,17 +113,17 @@ func (s *Schedule) ExecuteSubject(w io.Writer, a *conf.Alert, st *State) error {
 }
 
 // E executes the given expression and returns a value with corresponding tags
-// to the context's tags. If no such result is found, the first result with nil
+// to the Context's tags. If no such result is found, the first result with nil
 // tags is returned. If no such result is found, nil is returned. The precision
 // of numbers is truncated for convienent display. Array expressions are not
 // supported.
-func (c *context) E(v string) (s string) {
+func (c *Context) E(v string) (s string) {
 	e, err := expr.New(v)
 	if err != nil {
 		log.Printf("%s: %v", v, err)
 		return
 	}
-	res, _, err := e.Execute(c.schedule.cache, nil)
+	res, _, err := e.Execute(c.Schedule.cache, nil)
 	if err != nil {
 		log.Printf("%s: %v", v, err)
 		return
