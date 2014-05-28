3c9b7b2ffc71c6547e1de62de21ed3ec2090d283
Matt Jibson <matt.jibson@gmail.com>
2014-05-28 12:18:03 -0400

    cmd/scollector: Document in godoc

--- cmd/scollector/README.md
+++ cmd/scollector/README.md
@@ -1,34 +1,11 @@
 # scollector
 
-OpenTSDB's tcollector for the v2 HTTP API
-
-[tcollector](https://github.com/OpenTSDB/tcollector) is OpenTSDB's data collection framework built for OpenTSDB 1.0. scollector aims to be tcollector for OpenTSDB 2.0.
-
-# usage
+## installation
 
 ```
 $ go get github.com/StackExchange/scollector
-$ scollector [-h your-tsdb-server.com]
 ```
 
-See `scollector -help` for other options.
+## documentation
 
-scollector is implemented in go and thus compiles into a single binary. No python. No installation of anything other than the single scollector binary. All collectors are compiled into scollector itself. (scollector does support external collectors, but your goal should be to use those temporarily until the go version is written.) scollector has native collectors for Linux, Darwin, and Windows. You can cross compile go from one architecture to another, so one build server is enough to build for any machine.
-
-scollector will, by default, report to the host `tsaf`, which you should configure on your local nameserver to point to your TSDB server. This makes it possible to run scollector correctly with zero configuration or command line flags.
-
-# warning
-
-1. scollector is in testing, and you should not depend on it for production use yet. We are using it internally at Stack Exchange, but we are still fixing bugs.
-1. Some collector details may be specific to our environment, and so may act weirdly in yours. These will eventually be moved out into config options so the defaults works well for everyone.
-1. An OpenTSDB 2.0 server is required, since it uses the HTTP API. You may have to enable chunked requests on your server (not sure about this).
-1. It is designed to be used with an upcoming and unreleased project we are working on (called TSAF), and so may not work in your environment.
-1. scollector (as of mid April 2014) sends gzipped data to OpenTSDB. This feature will be available by default with OpenTSDB 2.1, but is not yet in the next branch. [A patch is available to enable this manually.](https://groups.google.com/d/msg/opentsdb/JQ7azVR5x_g/37yLYukU5R4J)
-
-# Windows
-
-scollector has full Windows support. It can be run standalone, or installed as a service (see --help for options). The Event Log is used when installed as a service.
-
-# snmp
-
-By default, scollector will collect data from the current host and report it to the TSDB server. scollector has an SNMP mode where it instead polls a given host: `scollector -s community@host`. Poll frequency currently defaults to 5 minutes. Some common OIDs regarding interfaces are collected. Others can be added easily.
+http://godoc.org/github.com/StackExchange/scollector
---
+++ cmd/scollector/doc.go
@@ -0,0 +1,67 @@
+/*
+
+Scollector is a metric collection agent for OpenTSDB 2.0.
+
+tcollector (https://github.com/OpenTSDB/tcollector) is OpenTSDB's data collection framework built for OpenTSDB 1.0. scollector aims to be tcollector for OpenTSDB 2.0.
+
+Unlike tcollector, scollector is a single binary with all collectors are compiled into scollector itself. scollector supports external collectors, but your goal should be to use those temporarily until the go version is written. It has native collectors for Linux, Darwin, and Windows.
+
+Usage:
+	scollector [flag]
+
+The flags are:
+	-h="tsaf"
+		OpenTSDB host; can optionally specify a port
+		("tsdb.example.com:4000"), but will default to 4242
+	-c=""
+		passthrough collector directory; t should contain numbered
+		directories like OpenTSDB tcollector expects; any executable
+		file in those directories is run every N seconds, where N is the
+		name of the directory; use 0 for a program that should be run
+		continuously and simply pass data through to OpenTSDB (the
+		program will be restarted if it exits; data output format is:
+		"metric timestamp value tag1=val1 tag2=val2 ..."; timestamp is
+		in Unix format (seconds since epoch); tags are optional; a host
+		tag is automatically added if not present
+	-s=""
+		SNMP host to poll of the format:
+		"community@host[,community@host...]"
+	-f=""
+		filter collectors matching this term (regex)
+	-t
+		test mode: run collectors once, print, and exit
+	-l
+		list enabled collectors
+	-p
+		print to screen instead of sending to a host
+	-f=0
+		generates X fake data points per second on the test.fake metric
+	-b=0
+		OpenTSDB batch size; used for debugging bad data
+
+Additional flags on Windows:
+	-winsvc=""
+		Windows Service management; can be: install, remove, start, stop
+
+scollector will, by default, report to the host `tsaf`, which you should configure on your local nameserver to point to your TSDB server. This makes it possible to run scollector correctly with zero configuration or command line flags.
+
+Warning
+
+scollector is in testing, and you should not depend on it for production use yet. We are using it internally at Stack Exchange, but we are still fixing bugs. It is designed to be used with an upcoming and unreleased project we are working on (called TSAF), and so may not work in your environment. Some collector details may be specific to our environment, and so may act weirdly in yours. These will eventually be moved out into config options so the defaults works well for everyone.
+
+An OpenTSDB 2.0 server is required, since it uses the HTTP API. You may have to enable chunked requests on your server (not sure about this). scollector sends gzipped data to OpenTSDB. This feature will be available by default with OpenTSDB 2.1, but is not yet in the next branch. A patch is available to enable this manually (https://groups.google.com/d/msg/opentsdb/JQ7azVR5x_g/37yLYukU5R4J).
+
+Windows
+
+scollector has full Windows support. It can be run standalone, or installed as a service (see -winsvc). The Event Log is used when installed as a service.
+
+SNMP
+
+By default, scollector will collect data from the current host and report it to the TSDB server. scollector has an SNMP mode where it instead polls a given host:
+
+	scollector -s community@host`
+
+Poll frequency currently defaults to 5 minutes. Some common OIDs regarding interfaces are collected. Others can be added easily.
+
+*/
+package main
