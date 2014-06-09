48f910f920a8a50b531f766e0ecd6194adbff4be
Kyle Brandt <kyle@kbrandt.com>
2014-06-09 16:43:41 -0500

    Should work, can't fully test because it seems that /api/put is broken

--- web/static/js/put.ts
+++ web/static/js/put.ts
@@ -8,14 +8,21 @@ class DP {
 	v: any;
 }
 
-interface IExprScope extends ng.IScope {
+interface IPutScope extends ng.IScope {
+	error: string;
+	running: string;
+	success: string;
 	metrics: string[];
 	metric: string;
 	tags: Tag[];
 	dps: DP[];
+	Submit: () => void;
+	GetTagKByMetric: () => void;
+	AddTag: () => void;
+	AddDP: () => void;
 }
 
-tsafControllers.controller('PutCtrl', ['$scope', '$http', '$route', function($scope: IExprScope, $http: ng.IHttpService, $route: ng.route.IRouteService) {
+tsafControllers.controller('PutCtrl', ['$scope', '$http', '$route', function($scope: IPutScope, $http: ng.IHttpService, $route: ng.route.IRouteService) {
 	var mfmt = 'YYYY/MM/DD-HH:mm:ss';
 	$scope.tags = [new Tag];
 	var dp = new DP;
@@ -28,17 +35,25 @@ tsafControllers.controller('PutCtrl', ['$scope', '$http', '$route', function($sc
 		.error(function(error) {
 			$scope.error = 'Unable to fetch metrics: ' + error;
 		});
-	$scope.Submit = function() {
+	$scope.Submit = () => {
 		var data: any = [];
 		var tags: any = {};
-		angular.forEach($scope.tags, function(v, k) {
-			if (v.k && v.v) {
+		angular.forEach($scope.tags, (v, k) => {
+			if (v.k || v.v) {
+				if (!v.k) {
+					$scope.error = 'Tag value ' + v.v + ' must have a key';
+					return;
+				}
+				if (!v.v) {
+					$scope.error = 'Tag key ' + v.k + ' must have a value';
+					return;
+				}
 				tags[v.k] =  v.v;
 			}
 		});
-		angular.forEach($scope.dps, function(v, k) {
+		angular.forEach($scope.dps, (v, k) => {
 			if (v.k && v.v) {
-				var ts = moment.utc(v.k, mfmt).format("X");
+				var ts = moment.utc(v.k, mfmt).format('X');
 				data.push({
 					metric: $scope.metric,
 					timestamp: ts,
@@ -47,23 +62,34 @@ tsafControllers.controller('PutCtrl', ['$scope', '$http', '$route', function($sc
 				});
 			}
 		});
-		console.log(JSON.stringify(data));
+		$scope.running = 'submitting data...';
+		$scope.success = '';
+		$scope.error = '';
+		$http.post('/api/put', data)
+			.success(() => {
+				$scope.running = '';
+				$scope.success = 'Data Submitted';
+				})
+			.error((error: any) => {
+					$scope.running = '';
+					$scope.error = error.error.message;
+				});
 	}
-	$scope.AddTag = function() {
+	$scope.AddTag = () => {
 		var last = $scope.tags[$scope.tags.length - 1];
 		if (last.k && last.v) {
 			$scope.tags.push(new Tag);
 		}
 	}
-	$scope.AddDP = function() {
+	$scope.AddDP = () => {
 		var last = $scope.dps[$scope.dps.length - 1];
 		if (last.k && last.v) {
 			var dp = new DP;
-			dp.k = moment.utc(last.k, mfmt).add("seconds", 15).format(mfmt);
+			dp.k = moment.utc(last.k, mfmt).add('seconds', 15).format(mfmt);
 			$scope.dps.push(dp);
 		}
 	}
-	$scope.GetTagKByMetric = function() {
+	$scope.GetTagKByMetric = () => {
 		$http.get('/api/tagk/' + $scope.metric)
 			.success(function(data: string[]) {
 				if (!angular.isArray(data)) {
