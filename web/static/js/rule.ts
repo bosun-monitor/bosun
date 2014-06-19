02468d499467140dcaad71ccc6ca1be854a4687e
Matt Jibson <matt.jibson@gmail.com>
2014-06-19 16:40:28 -0400

    Simpler rule page API
    Pass expr and it'll fill in a nice default rule and template.

--- web/static/js/rule.ts
+++ web/static/js/rule.ts
@@ -20,8 +20,9 @@ interface IRuleScope extends IExprScope {
 
 bosunControllers.controller('RuleCtrl', ['$scope', '$http', '$location', '$route', function($scope: IRuleScope, $http: ng.IHttpService, $location: ng.ILocationService, $route: ng.route.IRouteService) {
 	var search = $location.search();
-	var current_alert = search.alert;
+	var current_alert = atob(search.alert || '');
 	var current_template = search.template;
+	var expr = atob(search.expr || '') || 'avg(q("avg:rate{counter,,1}:os.cpu{host=*}", "5m", "")) > 10';
 	var status_map: any = {
 		"normal": 0,
 		"warning": 1,
@@ -30,26 +31,14 @@ bosunControllers.controller('RuleCtrl', ['$scope', '$http', '$location', '$route
 	$scope.date = search.date || '';
 	$scope.time = search.time || '';
 	$scope.tab = search.tab || 'results';
-	$http.get('/api/templates')
-		.success((data) => {
-			$scope.alerts = data.Alerts;
-			$scope.templates = data.Templates;
-		})
-	try {
-		current_alert = atob(current_alert);
-	}
-	catch (e) {
-		current_alert = '';
-	}
 	if (!current_alert) {
 		var alert_def =
 			'alert test {\n' +
 			'    template = test\n' +
-			'    $t = "5m"\n' +
-			'    $q = avg(q("avg:rate{counter,,1}:os.cpu{host=*}", $t, ""))\n' +
-			'    crit = $q > 10\n' +
+			'    crit = ' + expr + '\n' +
 			'}'
 		$location.search('alert', btoa(alert_def));
+		$location.search('expr', null);
 		return;
 	}
 	$scope.alert = current_alert;
@@ -62,9 +51,15 @@ bosunControllers.controller('RuleCtrl', ['$scope', '$http', '$location', '$route
 	if (!current_template) {
 		var template_def =
 			'template test {\n' +
-			'    body = `<h1>Name: {{.Alert.Name}}</h1>`\n' +
-			'    subject = `{{.Last.Status}}: {{.Alert.Name}}: {{.E .Alert.Vars.q}} on {{.Group.host}}`\n' +
-			'}'
+			'    subject = {{.Last.Status}}: {{.Alert.Name}} on {{.Group.host}}\n' +
+			'    body = `<p>Name: {{.Alert.Name}}\n' +
+			'    <p>Tags:\n' +
+			'    <table>\n' +
+			'        {{range $k, $v := .Group}}\n' +
+			'            <tr><td>{{$k}}</td><td>{{$v}}</td></tr>\n' +
+			'        {{end}}\n' +
+			'    </table>`\n' +
+			'}';
 		$location.search('template', btoa(template_def));
 		return;
 	}
@@ -124,5 +119,10 @@ bosunControllers.controller('RuleCtrl', ['$scope', '$http', '$location', '$route
 	$scope.zws = (v: string) => {
 		return v.replace(/([,{}()])/g, '$1\u200b');
 	};
+	$http.get('/api/templates')
+		.success((data) => {
+			$scope.alerts = data.Alerts;
+			$scope.templates = data.Templates;
+		});
 	$scope.set();
 }]);
