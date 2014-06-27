a71e2ce2ec11fc077299ef162bf7630a080da09a
Matt Jibson <matt.jibson@gmail.com>
2014-06-27 11:24:48 -0400

    cmd/scollector: Windows version and net iface metadata

---
+++ cmd/scollector/metadata/metadata_windows.go
@@ -0,0 +1,38 @@
+package metadata
+
+import (
+	"strings"
+
+	"github.com/StackExchange/scollector/opentsdb"
+	"github.com/StackExchange/scollector/util"
+)
+
+func init() {
+	metafuncs = append(metafuncs, metaWindowsVersion, metaWindowsIfaces)
+}
+
+func metaWindowsVersion() {
+	util.ReadCommand(func(line string) {
+		fields := strings.Fields(line)
+		if len(fields) == 0 {
+			return
+		}
+		AddMeta("", nil, "version", strings.Join(fields, " "))
+	}, "cmd", "/c", "ver")
+}
+
+func metaWindowsIfaces() {
+	var iface string
+	util.ReadCommand(func(line string) {
+		fields := strings.Fields(line)
+		sp := strings.Split(line, ":")
+		if len(fields) == 0 || len(sp) != 2 {
+			return
+		}
+		if line[0] != ' ' {
+			iface = sp[0]
+		} else if strings.HasPrefix(line, "   IPv4 Address") {
+			AddMeta("", opentsdb.TagSet{"iface": iface}, "addr", strings.TrimSpace(sp[1]))
+		}
+	}, "ipconfig")
+}
