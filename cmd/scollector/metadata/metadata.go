89d0f1c12b475211f3fde7de061765fabe3ae618
Matt Jibson <matt.jibson@gmail.com>
2014-06-30 16:10:39 -0400

    cmd/scollector: Move omreport metadata to its own file

--- cmd/scollector/metadata/metadata.go
+++ cmd/scollector/metadata/metadata.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"net/http"
 	"reflect"
-	"strings"
 	"sync"
 	"time"
 
@@ -95,25 +94,6 @@ func collectMetadata() {
 		sendMetadata()
 		time.Sleep(time.Hour)
 	}
-}
-
-func init() {
-	metafuncs = append(metafuncs, collectMetadataOmreport)
-}
-
-func collectMetadataOmreport() {
-	util.ReadCommand(func(line string) {
-		fields := strings.Split(line, ";")
-		if len(fields) != 2 {
-			return
-		}
-		switch fields[0] {
-		case "Chassis Service Tag":
-			AddMeta("", nil, "svctag", fields[1], true)
-		case "Chassis Model":
-			AddMeta("", nil, "model", fields[1], true)
-		}
-	}, "omreport", "chassis", "info", "-fmt", "ssv")
 }
 
 type Metasend struct {
