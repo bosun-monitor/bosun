01cd7f17cc4269021e46212da33e27d353dcdad1
Matt Jibson <matt.jibson@gmail.com>
2014-07-07 18:12:55 -0400

    cmd/scollector: tsaf -> bosun

--- cmd/scollector/main.go
+++ cmd/scollector/main.go
@@ -23,7 +23,7 @@ var (
 	flagTest      = flag.Bool("t", false, "Test - run collectors once, print, and exit.")
 	flagList      = flag.Bool("l", false, "List")
 	flagPrint     = flag.Bool("p", false, "Print to screen instead of sending to a host")
-	flagHost      = flag.String("h", "tsaf", `OpenTSDB host. Ex: "tsdb.example.com". Can optionally specify port: "tsdb.example.com:4000", but will default to 4242 otherwise`)
+	flagHost      = flag.String("h", "bosun", `OpenTSDB host. Ex: "tsdb.example.com". Can optionally specify port: "tsdb.example.com:4000", but will default to 4242 otherwise`)
 	flagColDir    = flag.String("c", "", `Passthrough collector directory. It should contain numbered directories like the OpenTSDB scollector expects. Any executable file in those directories is run every N seconds, where N is the name of the directory. Use 0 for a program that should be run continuously and simply pass data through to OpenTSDB (the program will be restarted if it exits. Data output format is: "metric timestamp value tag1=val1 tag2=val2 ...". Timestamp is in Unix format (seconds since epoch). Tags are optional. A host tag is automatically added, but overridden if specified.`)
 	flagBatchSize = flag.Int("b", 0, "OpenTSDB batch size. Used for debugging bad data.")
 	flagSNMP      = flag.String("s", "", "SNMP host to poll of the format: \"community@host[,community@host...]\".")
