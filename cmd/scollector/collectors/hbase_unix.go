78654a0acb9c183ca414d42431976f15b688ea07
Kyle Brandt <kyle@kbrandt.com>
2014-07-17 17:11:05 -0500

    cmd/scollector: Monitor hbase replication

--- cmd/scollector/collectors/hbase_unix.go
+++ cmd/scollector/collectors/hbase_unix.go
@@ -4,7 +4,9 @@ package collectors
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"strings"
 	"sync"
 	"time"
 
@@ -14,34 +16,46 @@ import (
 
 func init() {
 	collectors = append(collectors, &IntervalCollector{F: c_hbase_region, init: hbrInit})
+	collectors = append(collectors, &IntervalCollector{F: c_hbase_replication, init: hbrepInit})
+}
+
+type enabled struct {
+	Enable bool
+	Lock   sync.Mutex
 }
 
 var (
-	hbaseEnable bool
-	hbaseLock   sync.Mutex
+	hbrEnable   enabled
+	hbrepEnable enabled
 )
 
-func hbaseEnabled() (b bool) {
-	hbaseLock.Lock()
-	b = hbaseEnable
-	hbaseLock.Unlock()
+func (e enabled) Enabled() (b bool) {
+	e.Lock.Lock()
+	b = e.Enable
+	e.Lock.Unlock()
 	return
 }
 
 const hbrURL = "http://localhost:60030/jmx?qry=hadoop:service=RegionServer,name=RegionServerStatistics"
+const hbrepURL = "http://localhost:60030/jmx?qry=hadoop:service=Replication,name=*"
 
-func hbrInit() {
+func testUrl(url string, e *enabled) func() {
 	update := func() {
-		resp, err := http.Get(hbrURL)
-		hbaseLock.Lock()
-		defer hbaseLock.Unlock()
+		resp, err := http.Get(url)
+		e.Lock.Lock()
+		defer e.Lock.Unlock()
 		if err != nil {
-			hbaseEnable = false
+			e.Enable = false
 			return
 		}
 		resp.Body.Close()
-		hbaseEnable = resp.StatusCode == 200
+		e.Enable = resp.StatusCode == 200
 	}
+	return update
+}
+
+func hbrInit() {
+	update := testUrl(hbrURL, &hbrEnable)
 	update()
 	go func() {
 		for _ = range time.Tick(time.Minute * 5) {
@@ -51,7 +65,7 @@ func hbrInit() {
 }
 
 func c_hbase_region() opentsdb.MultiDataPoint {
-	if !hbaseEnabled() {
+	if !hbrEnable.Enabled() {
 		return nil
 	}
 	var md opentsdb.MultiDataPoint
@@ -75,6 +89,59 @@ func c_hbase_region() opentsdb.MultiDataPoint {
 				Add(&md, "hbase.region."+k, v, nil)
 			}
 		}
+	}
+	return md
+}
+
+func hbrepInit() {
+	update := testUrl(hbrepURL, &hbrepEnable)
+	update()
+	go func() {
+		for _ = range time.Tick(time.Minute * 5) {
+			update()
+		}
+	}()
+}
+
+func c_hbase_replication() opentsdb.MultiDataPoint {
+	if !hbrepEnable.Enabled() {
+		return nil
+	}
+	var md opentsdb.MultiDataPoint
+	res, err := http.Get(hbrepURL)
+	if err != nil {
+		slog.Errorln(err)
+		return nil
+	}
+	defer res.Body.Close()
+	var r struct {
+		Beans []map[string]interface{} `json:"beans"`
+	}
+	j := json.NewDecoder(res.Body)
+	if err := j.Decode(&r); err != nil {
+		slog.Errorln(err)
+		return nil
+	}
+	for i, section := range r.Beans {
+		var instance string
+		for k, v := range section {
+			if k == "name" && strings.HasPrefix(v.(string), "hadoop:service=Replication,name=ReplicationSource for") {
+				s := strings.Split(v.(string), " ")
+				fmt.Println(s[len(s)-1])
+				instance = s[len(s)-1]
+				continue
+			}
+		}
+		for k, v := range r.Beans[i] {
+			if _, ok := v.(float64); ok {
+				if instance == "" {
+					Add(&md, "hbase.replication."+k, v, nil)
+					continue
+				}
+				Add(&md, "hbase.replication."+k, v, opentsdb.TagSet{"instance": instance})
+			}
+		}
+		instance = ""
 	}
 	return md
 }
