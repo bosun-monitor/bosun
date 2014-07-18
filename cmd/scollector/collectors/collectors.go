6d28ddd70b5db0e36632f56b83fe950f067ab7e2
Kyle Brandt <kyle@kbrandt.com>
2014-07-18 18:26:35 -0500

    cmd/scollector: AddTS is like Add but you can specify a unix ts as an int64

--- cmd/scollector/collectors/collectors.go
+++ cmd/scollector/collectors/collectors.go
@@ -116,6 +116,13 @@ func Add(md *opentsdb.MultiDataPoint, name string, value interface{}, tags opent
 	}
 }
 
+func AddTS(md *opentsdb.MultiDataPoint, name string, ts int64, value interface{}, tags opentsdb.TagSet, rate metadata.RateType, unit metadata.Unit, desc string) {
+	Add(md, name, value, tags, rate, unit, desc)
+	for _, m := range *md {
+		m.Timestamp = ts
+	}
+}
+
 func readLine(fname string, line func(string)) error {
 	f, err := os.Open(fname)
 	if err != nil {
