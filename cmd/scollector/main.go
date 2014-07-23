7fb0e60667d7204615ebd5524f5c3a8341f0b55a
Matt Jibson <matt.jibson@gmail.com>
2014-07-23 18:58:28 -0400

    cmd/scollector: Move log setting up top

--- cmd/scollector/main.go
+++ cmd/scollector/main.go
@@ -91,6 +91,9 @@ func readConf() {
 
 func main() {
 	flag.Parse()
+	if *flagTest || *flagPrint {
+		slog.Set(&slog.StdLog{Log: log.New(os.Stdout, "", log.LstdFlags)})
+	}
 	for _, m := range mains {
 		m()
 	}
@@ -100,9 +103,6 @@ func main() {
 	util.Set()
 	if *flagColDir != "" {
 		collectors.InitPrograms(*flagColDir)
-	}
-	if *flagTest || *flagPrint {
-		slog.Set(&slog.StdLog{Log: log.New(os.Stdout, "", log.LstdFlags)})
 	}
 	if *flagSNMP != "" {
 		for _, s := range strings.Split(*flagSNMP, ",") {
