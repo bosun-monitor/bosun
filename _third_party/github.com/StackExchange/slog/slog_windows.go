0da61d9ffa5c3ca3ec4bc259054ab21a71f65e6e
Kyle Brandt <kyle@kbrandt.com>
2014-07-24 13:01:40 -0500

    Put back slog

---
+++ _third_party/github.com/StackExchange/slog/slog_windows.go
@@ -0,0 +1,30 @@
+package slog
+
+import "github.com/StackExchange/bosun/_third_party/code.google.com/p/winsvc/debug"
+
+type eventLog struct {
+	l  debug.Log
+	id uint32
+}
+
+// Sets the logger to a Windows Event Log. Designed for use with the
+// code.google.com/p/winsvc/eventlog and code.google.com/p/winsvc/debug
+// packages.
+func SetEventLog(l debug.Log, eid uint32) {
+	Set(&eventLog{l, eid})
+}
+
+func (e *eventLog) Fatal(v string) {
+	e.Error(v)
+}
+
+func (e *eventLog) Info(v string) {
+	e.l.Info(e.id, v)
+}
+
+func (e *eventLog) Warning(v string) {
+	e.l.Warning(e.id, v)
+}
+func (e *eventLog) Error(v string) {
+	e.l.Error(e.id, v)
+}
