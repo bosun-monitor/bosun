53e3e4b513fff9fa4d14ea9c582c3ba7b7856d8a
Kyle Brandt <kyle@kbrandt.com>
2014-07-24 13:26:27 -0500

    Ping hosts

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"log"
+	"net"
 	_ "net/http/pprof"
 	"os"
 	"path/filepath"
@@ -10,15 +11,19 @@ import (
 	"time"
 
 	"github.com/StackExchange/bosun/_third_party/github.com/StackExchange/scollector/collect"
+	"github.com/StackExchange/bosun/_third_party/github.com/StackExchange/scollector/opentsdb"
 	"github.com/StackExchange/bosun/_third_party/github.com/howeyc/fsnotify"
+	"github.com/StackExchange/bosun/_third_party/github.com/tatsushid/go-fastping"
 	"github.com/StackExchange/bosun/conf"
 	"github.com/StackExchange/bosun/sched"
+	"github.com/StackExchange/bosun/search"
 	"github.com/StackExchange/bosun/web"
 )
 
 var (
 	flagConf  = flag.String("c", "dev.conf", "config file location")
 	flagTest  = flag.Bool("t", false, "Only validate config then exit")
+	flagPing  = flag.Bool("p", true, "Pings known hosts")
 	flagWatch = flag.Bool("w", false, "watch current directory and exit on changes; for use with an autorestarter")
 )
 
@@ -32,6 +37,9 @@ func main() {
 	if *flagTest {
 		log.Println("Valid Config")
 		os.Exit(0)
+	}
+	if *flagPing {
+		go pingHosts()
 	}
 	if err := collect.Init(c.RelayListen, "bosun"); err != nil {
 		log.Fatal(err)
@@ -71,4 +79,46 @@ func watcher() {
 		watcher.Watch(path)
 		return nil
 	})
+}
+
+const pingFreq = time.Second * 15
+
+func pingHosts() {
+	hostmap := make(map[string]bool)
+	for {
+		hosts := search.TagValuesByTagKey("host")
+		for _, host := range hosts {
+			if _, ok := hostmap[host]; !ok {
+				hostmap[host] = true
+				go pingHost(host)
+			}
+		}
+		time.Sleep(pingFreq)
+	}
+}
+
+func pingHost(host string) {
+	for {
+		p := fastping.NewPinger()
+		ra, err := net.ResolveIPAddr("ip4:icmp", host)
+		if err != nil {
+			log.Print(err)
+			collect.Put("ping.resolved", opentsdb.TagSet{"dst_host": host}, 0)
+			time.Sleep(pingFreq)
+			continue
+		}
+		collect.Put("ping.resolved", opentsdb.TagSet{"dst_host": host}, 1)
+		p.AddIPAddr(ra)
+		p.MaxRTT = time.Second * 5
+		timeout := 1
+		p.AddHandler("receive", func(addr *net.IPAddr, t time.Duration) {
+			collect.Put("ping.rtt", opentsdb.TagSet{"dst_host": host}, float64(t)/float64(time.Millisecond))
+			timeout = 0
+		})
+		if err := p.Run(); err != nil {
+			log.Print(err)
+		}
+		collect.Put("ping.timeout", opentsdb.TagSet{"dst_host": host}, float64(timeout))
+		time.Sleep(pingFreq)
+	}
 }
