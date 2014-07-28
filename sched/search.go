da431d0c562aa7b038d56f46b3156c066531b645
Matt Jibson <matt.jibson@gmail.com>
2014-07-28 15:01:39 -0400

    party

--- sched/search.go
+++ sched/search.go
@@ -7,8 +7,8 @@ import (
 	"strings"
 	"sync"
 
+	"github.com/StackExchange/bosun/_third_party/github.com/StackExchange/scollector/collect"
 	"github.com/StackExchange/bosun/_third_party/github.com/StackExchange/scollector/opentsdb"
-	"github.com/StackExchange/scollector/collect"
 )
 
 // Search is a struct to hold indexed data about OpenTSDB metric and tag data.
