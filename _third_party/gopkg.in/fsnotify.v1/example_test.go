b393b5802526f501336ab93fd6d9f035f2fd3f65
Matt Jibson <matt.jibson@gmail.com>
2014-08-26 14:07:27 -0400

    Watch .go and .ts files separately

--- _third_party/gopkg.in/fsnotify.v1/example_test.go
+++ _third_party/gopkg.in/fsnotify.v1/example_test.go
@@ -2,12 +2,14 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// +build !plan9,!solaris
+
 package fsnotify_test
 
 import (
 	"log"
 
-	"github.com/StackExchange/bosun/_third_party/github.com/howeyc/fsnotify"
+	"github.com/StackExchange/bosun/_third_party/gopkg.in/fsnotify.v1"
 )
 
 func ExampleNewWatcher() {
@@ -15,19 +17,23 @@ func ExampleNewWatcher() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer watcher.Close()
 
 	go func() {
 		for {
 			select {
-			case ev := <-watcher.Event:
-				log.Println("event:", ev)
-			case err := <-watcher.Error:
+			case event := <-watcher.Events:
+				log.Println("event:", event)
+				if event.Op&fsnotify.Write == fsnotify.Write {
+					log.Println("modified file:", event.Name)
+				}
+			case err := <-watcher.Errors:
 				log.Println("error:", err)
 			}
 		}
 	}()
 
-	err = watcher.Watch("/tmp/foo")
+	err = watcher.Add("/tmp/foo")
 	if err != nil {
 		log.Fatal(err)
 	}
