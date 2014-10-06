28dfe07fb001c517bb90178b08ba08f7ee11ba1d
Greg Bray <Greg.Bray@Gmail.com>
2014-10-06 12:05:49 -0600

    cmd/scollector: Fixes issues mentioned on pull request

--- cmd/scollector/collectors/system_windows.go
+++ cmd/scollector/collectors/system_windows.go
@@ -19,10 +19,11 @@ func c_system_windows() (opentsdb.MultiDataPoint, error) {
 	}
 	var md opentsdb.MultiDataPoint
 	for _, v := range dst {
-		var uptime = (v.Timestamp_Object - v.SystemUpTime) / v.Frequency_Object //see http://microsoft.public.win32.programmer.wmi.narkive.com/09kqthVC/lastbootuptime
-		Add(&md, "win.system.uptime", uptime, nil, metadata.Gauge, metadata.None, "Seconds since last reboot.")
-		Add(&md, "win.system.processes", v.Processes, nil, metadata.Gauge, metadata.None, "Total running process count.")
-		Add(&md, osSystemUptime, uptime, nil, metadata.Gauge, metadata.None, "Seconds since last reboot.")
+		//see http://microsoft.public.win32.programmer.wmi.narkive.com/09kqthVC/lastbootuptime
+		var uptime = (v.Timestamp_Object - v.SystemUpTime) / v.Frequency_Object
+		Add(&md, "win.system.uptime", uptime, nil, metadata.Gauge, metadata.Second, osSystemUptimeDesc)
+		Add(&md, "win.system.processes", v.Processes, nil, metadata.Gauge, metadata.Count, "Total running process count.")
+		Add(&md, osSystemUptime, uptime, nil, metadata.Gauge, metadata.Second, osSystemUptimeDesc)
 	}
 	return md, nil
 }
