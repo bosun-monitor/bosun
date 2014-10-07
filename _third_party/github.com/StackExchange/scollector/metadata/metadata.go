c562cbe8395538aca979e5af0f54fd508359eb37
Matt Jibson <matt.jibson@gmail.com>
2014-10-07 14:01:41 -0400

    Update and show metadata timestamps

--- _third_party/github.com/StackExchange/scollector/metadata/metadata.go
+++ _third_party/github.com/StackExchange/scollector/metadata/metadata.go
@@ -116,6 +116,7 @@ type Metasend struct {
 	Tags   opentsdb.TagSet `json:",omitempty"`
 	Name   string          `json:",omitempty"`
 	Value  interface{}
+	Time   time.Time `json:",omitempty"`
 }
 
 func sendMetadata() {
