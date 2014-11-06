18f249e709a0cd0c6cdfc7f5dd477e341635b403
Matt Jibson <matt.jibson@gmail.com>
2014-11-06 17:55:59 -0500

    Remove hard-to-see silence placeholders
    fixes #383

--- web/static/js/bosun.js
+++ web/static/js/bosun.js
@@ -1894,6 +1894,9 @@ bosunControllers.controller('SilenceCtrl', ['$scope', '$http', '$location', '$ro
     $scope.hosts = search.hosts;
     $scope.tags = search.tags;
     $scope.edit = search.edit;
+    if (!$scope.end && !$scope.duration) {
+        $scope.duration = '1h';
+    }
     function get() {
         $http.get('/api/silence/get').success(function (data) {
             $scope.silences = data;
