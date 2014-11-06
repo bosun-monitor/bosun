b63972adad634dc2455d757d06309ffdae264b7b
Matt Jibson <matt.jibson@gmail.com>
2014-11-06 14:57:09 -0500

    slog: Use better syslog facility and severity levels

--- slog/slog_unix.go
+++ slog/slog_unix.go
@@ -2,16 +2,34 @@
 
 package slog
 
-import (
-	"log"
-	"log/syslog"
-)
+import "log/syslog"
 
 func SetSyslog() error {
-	l, err := syslog.NewLogger(syslog.LOG_NOTICE, log.LstdFlags)
+	w, err := syslog.New(syslog.LOG_LOCAL6, "")
 	if err != nil {
 		return err
 	}
-	Set(&StdLog{Log: l})
+	Set(&Syslog{W: w})
 	return nil
+}
+
+type Syslog struct {
+	W *syslog.Writer
+}
+
+func (s *Syslog) Fatal(v string) {
+	s.W.Crit("crit: " + v)
+}
+
+func (s *Syslog) Error(v string) {
+	s.W.Err("error: " + v)
+}
+
+func (s *Syslog) Info(v string) {
+	// Mac OSX ignores levels info and debug by default, so use notice.
+	s.W.Notice("info: " + v)
+}
+
+func (s *Syslog) Warning(v string) {
+	s.W.Warning("warning: " + v)
 }
