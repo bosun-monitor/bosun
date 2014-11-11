9cd746f67a046d4ec4b10b3c426eb13921b3e4f0
Greg Bray <Greg.Bray@Gmail.com>
2014-11-10 22:39:52 -0700

    cmd/scollector: Add network name and description to windows metadata

--- cmd/scollector/metadata/metadata_windows.go
+++ cmd/scollector/metadata/metadata_windows.go
@@ -1,12 +1,9 @@
 package metadata
 
 import (
-	"strings"
-
-	"github.com/bosun-monitor/scollector/opentsdb"
-	"github.com/bosun-monitor/scollector/util"
 	"github.com/StackExchange/slog"
 	"github.com/StackExchange/wmi"
+	"github.com/bosun-monitor/scollector/opentsdb"
 )
 
 func init() {
@@ -33,18 +30,54 @@ type Win32_OperatingSystem struct {
 }
 
 func metaWindowsIfaces() {
-	var iface string
-	util.ReadCommand(func(line string) error {
-		fields := strings.Fields(line)
-		sp := strings.Split(line, ":")
-		if len(fields) == 0 || len(sp) != 2 {
-			return nil
+	var dstConfigs []Win32_NetworkAdapterConfiguration
+	q := wmi.CreateQuery(&dstConfigs, "")
+	err := wmi.Query(q, &dstConfigs)
+	if err != nil {
+		slog.Error(err)
+		return
+	}
+
+	mNicConfigs := make(map[string]*Win32_NetworkAdapterConfiguration)
+	for i, nic := range dstConfigs {
+		mNicConfigs[nic.SettingID] = &dstConfigs[i]
+	}
+
+	var dstAdapters []MSFT_NetAdapter
+	q = wmi.CreateQuery(&dstAdapters, "WHERE HardwareInterface = True") //Exclude virtual adapters
+	err = wmi.QueryNamespace(q, &dstAdapters, "root\\StandardCimv2")
+	if err != nil {
+		slog.Error(err)
+		return
+	}
+
+	for _, v := range dstAdapters {
+		tag := opentsdb.TagSet{"iface": v.InterfaceName}
+		AddMeta("", tag, "description", v.InterfaceDescription, true)
+		AddMeta("", tag, "name", v.Name, true)
+		AddMeta("", tag, "speed", v.Speed, true)
+
+		nicConfig := mNicConfigs[v.InterfaceGuid]
+		if nicConfig != nil {
+			AddMeta("", tag, "mac", v.InterfaceGuid, true) // should be nicConfig.MACAddress
+			//for _, ip := range nic.IPAddress {
+			AddMeta("", tag, "addr", nicConfig.SettingID, true) // should be ip
+			//}
 		}
-		if line[0] != ' ' {
-			iface = sp[0]
-		} else if strings.HasPrefix(line, "   IPv4 Address") {
-			AddMeta("", opentsdb.TagSet{"iface": iface}, "addr", strings.TrimSpace(sp[1]), true)
-		}
-		return nil
-	}, "ipconfig")
+	}
+}
+
+type MSFT_NetAdapter struct {
+	Name                 string //NY-WEB09-PRI-NIC-A
+	Speed                uint64 //Bits per Second
+	InterfaceDescription string //Intel(R) Gigabit ET Quad Port Server Adapter #2
+	InterfaceName        string //Ethernet_10
+	InterfaceGuid        string //unique id
+}
+
+type Win32_NetworkAdapterConfiguration struct {
+	//IPAddress  []string //Both IPv4 and IPv6
+	//MACAddress string //00:1B:21:93:00:00
+	SettingID string //Matches InterfaceGuid
+	Caption   string
 }
