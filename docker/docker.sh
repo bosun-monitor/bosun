07c1afebd8636d621ec38d7e25a441f6a59fc09d
Matt Jibson <matt.jibson@gmail.com>
2014-11-14 15:57:13 -0500

    Prevent building docker images with cached binaries
    We always want new scollector and bosun binaries, so cache break them.

--- docker/docker.sh
+++ docker/docker.sh
@@ -7,10 +7,11 @@ if [ -x "$boot2docker" ]; then
 	boot2docker ssh sudo ntpclient -s -h pool.ntp.org
 fi
 
+docker build -t bosun-builder builder
 TMP=run/tmp
 rm -rf build/bosun $TMP
 git clone -b master --single-branch .. build/bosun
-docker build -t bosun-build build
+docker build -t bosun-build --no-cache build
 ID=$(docker run -d bosun-build)
 mkdir -p $TMP/hbase $TMP/bosun $TMP/tsdb $TMP/scollector
 docker cp ${ID}:/go/bin/bosun $TMP/bosun/.
