648f14a0f9dd31dcf00d01961f3621925bcb6e29
Matt Jibson <matt.jibson@gmail.com>
2014-11-17 15:49:24 -0500

    Correct statik command

--- w.sh
+++ w.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 while true; do
-	statik -src=web/static -dest=web
+	statik -src=web/static -dest=web -pkg=github.com/bosun-monitor/bosun/_third_party/github.com/rakyll/statik/fs
 	go run main.go -w -dev -r -q
 	if [ $? != 0 ] ; then
 		exit
