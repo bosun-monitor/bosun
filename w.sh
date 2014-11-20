5e56f51d03370b43455641022328029425e4ed8a
Matt Jibson <matt.jibson@gmail.com>
2014-11-20 15:45:47 -0500

    Run esc on quit so it (hopefully) runs after tsc

--- w.sh
+++ w.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 while true; do
-	esc -o web/static.go -pkg web -prefix web/static web/static/
 	go run main.go -w -dev -r -q
+	esc -o web/static.go -pkg web -prefix web/static web/static/
 	if [ $? != 0 ] ; then
 		exit
 	fi
