2dea93a7c13bc5c5f09087ddfb36f9b6f2656432
Matt Jibson <matt.jibson@gmail.com>
2014-11-21 14:14:20 -0500

    Revert esc change, just run on exit

--- w.sh
+++ w.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 while true; do
-	go run main.go -w -dev -r -q
 	esc -o web/static.go -pkg web -prefix web/static web/static/
+	go run main.go -w -dev -r -q
 	if [ $? != 0 ] ; then
+		esc -o web/static.go -pkg web -prefix web/static web/static/
 		exit
 	fi
 	echo restarting
