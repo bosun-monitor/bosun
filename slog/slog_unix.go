ec3009d5876068fa46fcc20373732553c3cea7eb
Matt Jibson <matt.jibson@gmail.com>
2014-12-08 17:07:17 -0500

    slog: golint cleanup

--- slog/slog_unix.go
+++ slog/slog_unix.go
@@ -4,6 +4,7 @@ package slog
 
 import "log/syslog"
 
+// SetSyslog configures slog to use the system syslog daemon.
 func SetSyslog() error {
 	w, err := syslog.New(syslog.LOG_LOCAL6, "")
 	if err != nil {
@@ -13,23 +14,28 @@ func SetSyslog() error {
 	return nil
 }
 
+// Syslog logs to syslog.
 type Syslog struct {
 	W *syslog.Writer
 }
 
+// Fatal logs a fatal message and calls os.Exit(1).
 func (s *Syslog) Fatal(v string) {
 	s.W.Crit("crit: " + v)
 }
 
+// Error logs an error message.
 func (s *Syslog) Error(v string) {
 	s.W.Err("error: " + v)
 }
 
+// Info logs an info message.
 func (s *Syslog) Info(v string) {
 	// Mac OSX ignores levels info and debug by default, so use notice.
 	s.W.Notice("info: " + v)
 }
 
+// Warning logs a warning message.
 func (s *Syslog) Warning(v string) {
 	s.W.Warning("warning: " + v)
 }
