d1ba2c350d1e3f64d44dcf194970f86687bb0a2f
Matt Jibson <matt.jibson@gmail.com>
2015-01-02 20:07:37 -0500

    cmd/bosun: Build docker image from local git repo

--- cmd/bosun/docker/docker.sh
+++ cmd/bosun/docker/docker.sh
@@ -2,15 +2,16 @@
 
 set -e
 
+branch=${1-master}
+
 if  type boot2docker >/dev/null 2>&1; then
         boot2docker ssh sudo ntpclient -s -h pool.ntp.org
 fi
 
-docker build -t bosun-builder builder
 TMP=run/tmp
 rm -rf build/bosun $TMP
-git clone -b master --single-branch ../../.. build/bosun
-docker build -t bosun-build --no-cache build
+git clone -b $branch --single-branch ../../.. build/bosun
+docker build -t bosun-build build
 ID=$(docker run -d bosun-build)
 mkdir -p $TMP/hbase $TMP/bosun $TMP/tsdb $TMP/scollector
 docker cp ${ID}:/go/bin/bosun $TMP/bosun/.
