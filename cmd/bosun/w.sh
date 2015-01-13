ad53b698bc1b8337423ae72c3dac05d6072be46c
Dieter Plaetinck <dieter@vimeo.com>
2015-01-13 09:09:20 -0500

    make w.sh simpler

--- cmd/bosun/w.sh
+++ cmd/bosun/w.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-while true; do
-	go run main.go -w -dev -r -q
-	if [ $? != 0 ] ; then
-		exit
-	fi
-	echo restarting
+while echo "(RE)STARTING BOSUN"; do
+	go run main.go -w -dev -r -q || exit
 done
