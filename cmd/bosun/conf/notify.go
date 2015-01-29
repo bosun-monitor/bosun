9c821ad1c2cc611c0ea60bf3e96b4e5a76f1ce0c
Dieter Plaetinck <dieter@vimeo.com>
2015-01-29 14:16:00 -0500

    clean up extraneous whitespace in email subjects
    otherwise the subjects can be unreadable due to
    whitespace introduced in templates

--- cmd/bosun/conf/notify.go
+++ cmd/bosun/conf/notify.go
@@ -78,7 +78,10 @@ func (n *Notification) DoEmail(subject, body []byte, c *Conf, ak string, attachm
 	for _, a := range n.Email {
 		e.To = append(e.To, a.Address)
 	}
-	e.Subject = string(subject)
+
+	// remove various extraneous whitespace that can get accidentally introduced by templates.
+	e.Subject = strings.Join(strings.Fields(string(subject)), " ")
+
 	e.HTML = body
 	for _, a := range attachments {
 		e.Attach(bytes.NewBuffer(a.Data), a.Filename, a.ContentType)
