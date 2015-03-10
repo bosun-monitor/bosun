8f8a83a895e4991ca5e8225ba75d38f58c0a259b
Peter Grace <pete.grace@gmail.com>
2015-03-10 12:42:22 -0600

    revert import alphabetization

--- cmd/scollector/collectors/aws.go
+++ cmd/scollector/collectors/aws.go
@@ -1,15 +1,17 @@
 package collectors
 
 import (
+	"fmt"
+	"time"
+
+	"bosun.org/metadata"
+	"bosun.org/opentsdb"
+	"bosun.org/slog"
+
 	"bosun.org/_third_party/github.com/awslabs/aws-sdk-go/aws"
 	"bosun.org/_third_party/github.com/awslabs/aws-sdk-go/gen/cloudwatch"
 	"bosun.org/_third_party/github.com/awslabs/aws-sdk-go/gen/ec2"
 	"bosun.org/_third_party/github.com/awslabs/aws-sdk-go/gen/elb"
-	"bosun.org/metadata"
-	"bosun.org/opentsdb"
-	"bosun.org/slog"
-	"fmt"
-	"time"
 )
 
 const (
