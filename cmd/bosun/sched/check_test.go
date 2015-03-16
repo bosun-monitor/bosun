bosun-monitor/bosun

commit 4b63814009068d893d002b41e8b780328a03f0b9

cmd/bosun: Fix silence logic
Fixes #769

cmd/bosun/sched/notify.go
@@ -89,12 +89,16 @@ func (s *Schedule) sendNotifications(silenced map[expr.AlertKey]Silence) {
 		ustates := make(States)
 		for _, st := range states {
 			ak := st.AlertKey()
+			_, silenced := silenced[ak]
 			if st.Last().Status == StUnknown {
-				if _, ok := silenced[ak]; ok {
+				if silenced {
 					log.Println("silencing unknown", ak)
 					continue
 				}
 				ustates[ak] = st
+			}
+			if silenced {
+				log.Println("silencing", ak)
 			} else {
 				s.notify(st, n)
 			}

cmd/bosun/sched/check_test.go
@@ -1,7 +1,12 @@
 package sched
 
 import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
 	"testing"
+	"time"
 
 	"bosun.org/cmd/bosun/conf"
 	"bosun.org/cmd/bosun/expr"
@@ -71,5 +76,54 @@ func TestCheckFlapping(t *testing.T) {
 	s.RunHistory(r)
 	if !hasNots() {
 		t.Fatal("expected notification")
+	}
+}
+
+func TestCheckSilence(t *testing.T) {
+	s := new(Schedule)
+	done := make(chan bool, 1)
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		done <- true
+	}))
+	defer ts.Close()
+	u, err := url.Parse(ts.URL)
+	if err != nil {
+		t.Fatal(err)
+	}
+	c, err := conf.New("", fmt.Sprintf(`
+		template t {
+			subject = "test"
+			body = "test"
+		}
+		notification n {
+			post = http://%s/
+		}
+		alert a {
+			template = t
+			warnNotification = n
+			warn = 1
+		}
+	`, u.Host))
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = s.Init(c)
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = s.AddSilence(time.Now().Add(-time.Hour), time.Now().Add(time.Hour), "a", "", false, true, "")
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = s.Check(nil, time.Now())
+	if err != nil {
+		t.Fatal(err)
+	}
+	s.CheckNotifications()
+	select {
+	case <-done:
+		t.Fatal("silenced notification was sent")
+	case <-time.After(time.Second * 2):
+		// Timeout *probably* means the silence worked
 	}
 }

commit 82c70781442a1d011fe31c7a7a9d8d6bf1965a1e

cmd/bosun: fix sending empty notifications for unknowns
4b63814009068d893d002b41e8b780328a03f0b9 introduced a bug because there
was just 1 if/else block, but it added a new if block before that. Thus,
in the unknown case, it would descend into both blocks and send an empty
notification.

cmd/bosun/sched/notify.go
@@ -96,8 +96,7 @@ func (s *Schedule) sendNotifications(silenced map[expr.AlertKey]Silence) {
 					continue
 				}
 				ustates[ak] = st
-			}
-			if silenced {
+			} else if silenced {
 				log.Println("silencing", ak)
 			} else {
 				s.notify(st, n)

cmd/bosun/sched/check_test.go
@@ -11,6 +11,7 @@ import (
 
 	"bosun.org/cmd/bosun/conf"
 	"bosun.org/cmd/bosun/expr"
+	"bosun.org/opentsdb"
 )
 
 func TestCheckFlapping(t *testing.T) {
@@ -187,5 +188,73 @@ func TestCheckNotify(t *testing.T) {
 		}
 	case <-time.After(time.Second):
 		t.Fatal("failed to receive notification before timeout")
+	}
+}
+
+func TestCheckNotifyUnknown(t *testing.T) {
+	s := new(Schedule)
+	nc := make(chan string, 1)
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		b, _ := ioutil.ReadAll(r.Body)
+		nc <- string(b)
+	}))
+	defer ts.Close()
+	u, err := url.Parse(ts.URL)
+	if err != nil {
+		t.Fatal(err)
+	}
+	c, err := conf.New("", fmt.Sprintf(`
+		template t {
+			subject = {{.Name}}: {{.Group | len}} unknown alerts
+			body = `+"`"+`
+			<p>Time: {{.Time}}
+			<p>Name: {{.Name}}
+			<p>Alerts:
+			{{range .Group}}
+				<br>{{.}}
+			{{end}}`+"`"+`
+		}
+		unknownTemplate = t
+		notification n {
+			post = http://%s/
+		}
+		alert a {
+			critNotification = n
+			crit = 1
+		}
+	`, u.Host))
+	if err != nil {
+		t.Fatal(err)
+	}
+	c.StateFile = ""
+	err = s.Init(c)
+	if err != nil {
+		t.Fatal(err)
+	}
+	r := &RunHistory{
+		Events: map[expr.AlertKey]*Event{
+			expr.NewAlertKey("a", opentsdb.TagSet{"h": "x"}): {Status: StUnknown},
+			expr.NewAlertKey("a", opentsdb.TagSet{"h": "y"}): {Status: StUnknown},
+		},
+	}
+	s.RunHistory(r)
+	s.CheckNotifications()
+	gotExpected := false
+Loop:
+	for {
+		select {
+		case r := <-nc:
+			if r == "a: 2 unknown alerts" {
+				gotExpected = true
+			} else {
+				t.Fatalf("unexpected: %v", r)
+			}
+		// TODO: remove this silly timeout-based test
+		case <-time.After(time.Second):
+			break Loop
+		}
+	}
+	if !gotExpected {
+		t.Errorf("didn't get expected result")
 	}
 }