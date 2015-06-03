fbfdf7de4272271eb5c32e1c971e5705f898939d
Craig Peterson <cpeterson@stackoverflow.com>
2015-06-03 15:25:48 -0600

    Allowing docs to be embedded and hosted in main app.

--- build/generate/generate.go
+++ build/generate/generate.go
@@ -1,10 +1,34 @@
+// This file intended only to be called by go generate directive with working directory in cmd/bosun
 package main
 
 import (
 	"bosun.org/cmd/bosun/web"
+	"log"
+	"os"
+	"os/exec"
 )
 
 func main() {
 	web.RunTsc()
 	web.RunEsc()
+
+	jekylCmd := exec.Command("jekyll", "build", "--source", "../../docs")
+	run(jekylCmd)
+
+	escCmd := exec.Command("esc", "-o", "web/docs/docs.go", "-prefix=_site", "-pkg=docs", "_site")
+	run(escCmd)
+
+	err := os.RemoveAll("_site")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run(cmd *exec.Cmd) {
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
