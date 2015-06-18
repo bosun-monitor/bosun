608214c2aa5ec8886df96e4dfc8dbce26e12aa67
Greg Bray <Greg.Bray@Gmail.com>
2015-06-18 16:00:15 -0600

    cmd/scollector: WIP for DSC status

--- cmd/scollector/collectors/dsc_windows.go
+++ cmd/scollector/collectors/dsc_windows.go
@@ -1,8 +1,14 @@
 package collectors
 
 import (
+	"fmt"
 	"os"
 	"time"
+
+	"bytes"
+
+	"bosun.org/_third_party/github.com/stackexchange/mof"
+	"bosun.org/util"
 
 	"bosun.org/metadata"
 	"bosun.org/opentsdb"
@@ -10,11 +16,13 @@ import (
 
 func init() {
 	collectors = append(collectors, &IntervalCollector{F: c_dsc_mof, Interval: time.Minute * 5})
+	collectors = append(collectors, &IntervalCollector{F: c_dsc_status, Interval: time.Minute * 5})
 }
 
 const (
-	dscLCM = "dsc.lcm."
-	dscMof = "dsc.mof."
+	dscLCM    = "dsc.lcm."
+	dscMof    = "dsc.mof."
+	dscStatus = "dsc.status."
 )
 
 var (
@@ -60,3 +68,134 @@ const (
 	descDSCMofSize       = "Size of the mof file in bytes or -1 if file does not exist."
 	descDSCMofModified   = "Number of seconds since the mof file was last modified or -1 if file does not exist."
 )
+
+func c_dsc_status() (opentsdb.MultiDataPoint, error) {
+	var md opentsdb.MultiDataPoint
+	if _, err := os.Stat(dscpath + "MetaConfig.mof"); os.IsNotExist(err) {
+		return md, nil
+	}
+	var dst MSFT_DSCConfigurationStatus
+	dscstatusmof, err := util.Command(time.Minute, nil, "wmic",
+		`/namespace:\\ROOT\Microsoft\Windows\DesiredStateConfiguration`, "class",
+		"MSFT_DSCLocalConfigurationManager", "call", "GetConfigurationStatus")
+	if err != nil {
+		return nil, err
+	}
+	dscstatusbuffer := new(bytes.Buffer)
+	_, err = dscstatusbuffer.ReadFrom(dscstatusmof)
+	if err != nil {
+		return nil, err
+	}
+	err = mof.Unmarshal(dscstatusbuffer.Bytes(), &dst)
+	if err != nil {
+		return nil, err
+	}
+	if dst.ReturnValue != 0 {
+		return nil, fmt.Errorf("GetConfigurationStatus ReturnValue %v", dst.ReturnValue)
+	}
+	for _, v := range dst.MSFT_DSCConfigurationStatus {
+		Add(&md, dscLCM+"refresh_mode", dscModeToStatusCode(v.Mode), nil, metadata.Gauge, metadata.StatusCode, descWinDSCMode)
+		Add(&md, dscStatus+"reboot_requested", v.RebootRequested, nil, metadata.Gauge, metadata.Bool, descWinDSCRebootRequested)
+		Add(&md, dscStatus+"resources_total", v.NumberOfResources, nil, metadata.Gauge, metadata.Count, descWinDSCNumberOfResources)
+		Add(&md, dscStatus+"run_age", dscStartDateToAge(v.StartDate), nil, metadata.Gauge, metadata.Second, descWinDSCAge)
+		Add(&md, dscStatus+"run_duration", v.DurationInSeconds, nil, metadata.Gauge, metadata.Second, descWinDSCDurationInSeconds)
+		Add(&md, dscStatus+"run_success", v.Status == "Success", nil, metadata.Gauge, metadata.Bool, descWinDSCStatus)
+		Add(&md, dscStatus+"run_type", dscTypeToStatusCode(v.Type), nil, metadata.Gauge, metadata.Count, descWinDSCType)
+		configurations := make(map[string]dscResourceCount)
+		for _, r := range v.ResourcesInDesiredState {
+			c := configurations[r.ConfigurationName]
+			c.Success++
+			configurations[r.ConfigurationName] = c
+		}
+		for _, r := range v.ResourcesNotInDesiredState {
+			c := configurations[r.ConfigurationName]
+			c.Failed++
+			configurations[r.ConfigurationName] = c
+		}
+		for key, value := range configurations {
+			Add(&md, dscStatus+"resources", value.Success, opentsdb.TagSet{"state": "Success", "configuration": key}, metadata.Gauge, metadata.Count, descWinDSCResourceState)
+			Add(&md, dscStatus+"resources", value.Failed, opentsdb.TagSet{"state": "Failed", "configuration": key}, metadata.Gauge, metadata.Count, descWinDSCResourceState)
+		}
+	}
+	return md, nil
+}
+
+const (
+	descWinDSCAge               = "Seconds since the last DSC run."
+	descWinDSCMode              = "Refresh mode of configuration. 0=Pull, 1=Push, 2=Disabled, -1=Unknown."
+	descWinDSCNumberOfResources = "Total number of resources in configuration."
+	descWinDSCRebootRequested   = "Reboot was requested during configuration run."
+	descWinDSCResourceState     = "Count of resources from each named configuration that were successful or failed."
+	descWinDSCStartDate         = "Date and time when the configuration was started."
+	descWinDSCStatus            = "Status of configuration."
+	descWinDSCType              = "Type of configuration run. 0=Reboot, 1=Initial, 2=Consistency, 3=ReadOnly, -1=Unknown."
+	descWinDSCDurationInSeconds = "Seconds taken to process entire configuration."
+)
+
+type MSFT_DSCConfigurationStatus struct {
+	MSFT_DSCConfigurationStatus []struct {
+		DurationInSeconds int
+		MetaConfiguration struct {
+			AllowModuleOverwrite           bool
+			ConfigurationModeFrequencyMins int
+			LCMState                       string
+			RebootNodeIfNeeded             bool
+			RefreshFrequencyMins           int
+			StatusRetentionTimeInDays      int
+		}
+		Mode                    string
+		NumberOfResources       int
+		RebootRequested         bool
+		ResourcesInDesiredState []struct {
+			ConfigurationName string
+		}
+		ResourcesNotInDesiredState []struct {
+			ConfigurationName string
+		}
+		StartDate string
+		Status    string
+		Type      string
+	}
+	ReturnValue int
+}
+
+type dscResourceCount struct {
+	Success int
+	Failed  int
+}
+
+func dscModeToStatusCode(mode string) int64 {
+	switch mode {
+	case "PULL":
+		return 0
+	case "PUSH":
+		return 1
+	case "DISABLED":
+		return 2
+	default:
+		return -1
+	}
+}
+
+func dscTypeToStatusCode(t string) int64 {
+	switch t {
+	case "Reboot":
+		return 0
+	case "Initial":
+		return 1
+	case "Consistency":
+		return 2
+	case "ReadOnly":
+		return 3
+	default:
+		return -1
+	}
+}
+
+func dscStartDateToAge(startdate string) float64 {
+	t, err := time.Parse("2006/01/02 15:04:05", startdate)
+	if err != nil {
+		return -1
+	}
+	return time.Now().Sub(t).Seconds()
+}
