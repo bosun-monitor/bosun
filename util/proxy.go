3b370172296679335eb9316d2031365584a7af4f
Craig Peterson <cpeterson@stackoverflow.com>
2015-09-16 14:14:59 -0600

    copying a smidgen less code out of httputil

--- util/proxy.go
+++ util/proxy.go
@@ -4,22 +4,14 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"net/url"
-	"path"
 )
 
 // Creates a new http Proxy that forwards requests to the specified url.
 // Differs from httputil.NewSingleHostReverseProxy only in that it properly sets the host header.
 func NewSingleHostProxy(target *url.URL) *httputil.ReverseProxy {
-	targetQuery := target.RawQuery
+	proxy := httputil.NewSingleHostReverseProxy(target)
 	director := func(req *http.Request) {
-		req.URL.Scheme = target.Scheme
-		req.URL.Host = target.Host
-		req.URL.Path = path.Join(target.Path, req.URL.Path)
-		if targetQuery == "" || req.URL.RawQuery == "" {
-			req.URL.RawQuery = targetQuery + req.URL.RawQuery
-		} else {
-			req.URL.RawQuery = targetQuery + "&" + req.URL.RawQuery
-		}
+		proxy.Director(req)
 		req.Host = target.Host
 	}
 	return &httputil.ReverseProxy{Director: director}
