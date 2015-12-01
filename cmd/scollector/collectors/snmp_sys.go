39a8c28234cf60577799d1a54a95ef6629e59196
Kyle Brandt <kyle@kbrandt.com>
2015-12-01 15:42:13 -0500

    cmd/scollector: Collect system uptime via snmp

---
+++ cmd/scollector/collectors/snmp_sys.go
@@ -0,0 +1,34 @@
+package collectors
+
+import (
+	"fmt"
+	"math/big"
+	"time"
+
+	"bosun.org/cmd/scollector/conf"
+	"bosun.org/metadata"
+	"bosun.org/opentsdb"
+)
+
+const sysUpTime = ".1.3.6.1.2.1.1.3.0" // "The time (in hundredths of a second) since the network management portion of the system was last re-initialized."
+
+// SNMPSys registers a SNMP system data collector for the given community and host.
+func SNMPSys(cfg conf.SNMP) {
+	collectors = append(collectors, &IntervalCollector{
+		F: func() (opentsdb.MultiDataPoint, error) {
+			return c_snmp_sys(cfg.Community, cfg.Host)
+		},
+		Interval: time.Minute * 1,
+		name:     fmt.Sprintf("snmp-sys-%s", cfg.Host),
+	})
+}
+
+func c_snmp_sys(community, host string) (opentsdb.MultiDataPoint, error) {
+	var md opentsdb.MultiDataPoint
+	uptime, err := snmp_oid(host, community, sysUpTime)
+	if err != nil {
+		return md, err
+	}
+	Add(&md, osSystemUptime, uptime.Int64()/big.NewInt(100).Int64(), opentsdb.TagSet{"host": host}, metadata.Gauge, metadata.Second, osSystemUptimeDesc)
+	return md, nil
+}
