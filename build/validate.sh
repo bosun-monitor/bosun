93810a9cc0e592b94960591bbc24b42bdf71d012
Craig Peterson <cpeterson@stackoverflow.com>
2016-01-25 19:51:14 -0700

    Update validate.sh

--- build/validate.sh
+++ build/validate.sh
@@ -57,6 +57,7 @@ if [ "$GOGENERATEDIFF" != '' ]; then
     echo "$GOGENERATEDIFF"
 	BUILDMSG="${BUILDMSG}go generate needs to run. "
     GOGENERATEDIFFRESULT=1
+    git diff
 fi
 
 echo -e "\nRunning go test bosun.org/..."
