2b67b7c53ac6215c796ffcbdbfddc22246663e91
Greg Bray <Greg.Bray@Gmail.com>
2016-02-24 15:13:42 -0700

    cmd/scollector: add hadoop collector after setting hb URLs
    Fixes #1564
    Closes #1567

--- cmd/scollector/collectors/hbase_unix.go
+++ cmd/scollector/collectors/hbase_unix.go
@@ -31,11 +31,10 @@ func init() {
 		hbURL = host + hbURL
 		hbRepURL = host + hbRepURL
 		hbGCURL = host + hbGCURL
-
+		collectors = append(collectors, &IntervalCollector{F: c_hbase_region, Enable: enableURL(hbURL)})
+		collectors = append(collectors, &IntervalCollector{F: c_hbase_replication, Enable: enableURL(hbRepURL)})
+		collectors = append(collectors, &IntervalCollector{F: c_hbase_gc, Enable: enableURL(hbGCURL)})
 	})
-	collectors = append(collectors, &IntervalCollector{F: c_hbase_region, Enable: enableURL(hbURL)})
-	collectors = append(collectors, &IntervalCollector{F: c_hbase_replication, Enable: enableURL(hbRepURL)})
-	collectors = append(collectors, &IntervalCollector{F: c_hbase_gc, Enable: enableURL(hbGCURL)})
 }
 
 type jmx struct {
