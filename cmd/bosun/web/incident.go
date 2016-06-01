6bf8c5af8596310d4de067a36f304eca098434ed
Kyle Brandt <kyle@kbrandt.com>
2016-06-01 11:53:21 -0400

    add ability to filter on start time (relative to now)

--- cmd/bosun/web/incident.go
+++ cmd/bosun/web/incident.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 	"strings"
 
+	"time"
+
 	"bosun.org/cmd/bosun/conf"
 	"bosun.org/models"
 	"bosun.org/opentsdb"
@@ -18,7 +20,6 @@ type IncidentSummary struct {
 	Id                     int64
 	Subject                string
 	Start                  int64
-	Alert                  string
 	AlertName              string
 	Tags                   opentsdb.TagSet
 	TagsString             string
@@ -36,7 +37,7 @@ type IncidentSummary struct {
 func (is IncidentSummary) Ask(filter string) (bool, error) {
 	sp := strings.SplitN(filter, ":", 2)
 	if len(sp) != 2 {
-		return false, fmt.Errorf("bad ask length: sp is %v", sp)
+		return false, fmt.Errorf("bad filter, filter must be in k:v format, got %v", filter)
 	}
 	key := sp[0]
 	value := sp[1]
@@ -53,7 +54,7 @@ func (is IncidentSummary) Ask(filter string) (bool, error) {
 	case "hasTag":
 		if strings.Contains(value, "=") {
 			if strings.HasPrefix(value, "=") {
-				q := strings.TrimLeft(value, "=")
+				q := strings.TrimPrefix(value, "=")
 				for _, v := range is.Tags {
 					if glob.Glob(q, v) {
 						return true, nil
@@ -62,7 +63,7 @@ func (is IncidentSummary) Ask(filter string) (bool, error) {
 				return false, nil
 			}
 			if strings.HasSuffix(value, "=") {
-				q := strings.TrimRight(value, "=")
+				q := strings.TrimSuffix(value, "=")
 				_, ok := is.Tags[q]
 				return ok, nil
 			}
@@ -80,6 +81,16 @@ func (is IncidentSummary) Ask(filter string) (bool, error) {
 		q := strings.TrimRight(value, "=")
 		_, ok := is.Tags[q]
 		return ok, nil
+	case "hidden":
+		hide := is.Silenced || is.Unevaluated
+		switch value {
+		case "true":
+			return hide == true, nil
+		case "false":
+			return hide == false, nil
+		default:
+			return false, fmt.Errorf("unknown %s value: %s", key, value)
+		}
 	case "name":
 		return glob.Glob(value, is.AlertName), nil
 	case "notify":
@@ -107,6 +118,44 @@ func (is IncidentSummary) Ask(filter string) (bool, error) {
 		default:
 			return false, fmt.Errorf("unknown %s value: %s", key, value)
 		}
+	case "start":
+		var op string
+		val := value
+		if strings.HasPrefix(value, "<") {
+			op = "<"
+			val = strings.TrimLeft(value, op)
+		}
+		if strings.HasPrefix(value, ">") {
+			op = ">"
+			val = strings.TrimLeft(value, op)
+		}
+		d, err := opentsdb.ParseDuration(val)
+		if err != nil {
+			return false, err
+		}
+		startTime := time.Unix(is.Start, 0)
+		// might want to make Now a property of incident summary for viewing things in the past
+		// but not going there at the moment. This is because right now I'm working with open
+		// incidents. And "What did incidents look like at this time?" is a different question
+		// since those incidents will no longer be open.
+		relativeTime := time.Now().UTC().Add(time.Duration(-d))
+		switch op {
+		case ">", "":
+			return startTime.After(relativeTime), nil
+		case "<":
+			return startTime.Before(relativeTime), nil
+		default:
+			return false, fmt.Errorf("unexpected op: %v", op)
+		}
+	case "unevaluated":
+		switch value {
+		case "true":
+			return is.Unevaluated == true, nil
+		case "false":
+			return is.Unevaluated == false, nil
+		default:
+			return false, fmt.Errorf("unknown %s value: %s", key, value)
+		}
 	case "status": // CurrentStatus
 		return is.CurrentStatus.String() == value, nil
 	case "worstStatus":
@@ -130,25 +179,21 @@ func ListOpenIncidents(t miniprofiler.Timer, w http.ResponseWriter, r *http.Requ
 		return nil, fmt.Errorf("failed to get silences")
 	}
 	summaries := []IncidentSummary{}
-	// filter, err := sched.MakeFilter(r.FormValue("filter"))
-	// if err != nil {
-	// 	return nil, fmt.Errorf("bad filter: %v", err)
-	// }
-	parsedExpr, err := parse.Parse(r.FormValue("filter"))
-	if err != nil {
-		return nil, fmt.Errorf("bad filter: %v", err)
+	filterText := r.FormValue("filter")
+	var parsedExpr *parse.Tree
+	if filterText != "" {
+		parsedExpr, err = parse.Parse(filterText)
+		if err != nil {
+			return nil, fmt.Errorf("bad filter: %v", err)
+		}
 	}
 	for _, iState := range list {
-		// if !filter(schedule.Conf, schedule.Conf.Alerts[iState.Alert], iState) {
-		// 	continue
-		// }
 		warnNotifications := schedule.Conf.Alerts[iState.AlertKey.Name()].WarnNotification.Get(schedule.Conf, iState.AlertKey.Group())
 		critNotifications := schedule.Conf.Alerts[iState.AlertKey.Name()].CritNotification.Get(schedule.Conf, iState.AlertKey.Group())
 		is := IncidentSummary{
 			Id:                     iState.Id,
 			Subject:                iState.Subject,
 			Start:                  iState.Start.Unix(),
-			Alert:                  iState.Alert,
 			AlertName:              iState.AlertKey.Name(),
 			Tags:                   iState.AlertKey.Group(),
 			TagsString:             iState.AlertKey.Group().String(),
@@ -161,6 +206,10 @@ func ListOpenIncidents(t miniprofiler.Timer, w http.ResponseWriter, r *http.Requ
 			Silenced:               suppressor(iState.AlertKey) != nil,
 			WarnNotificationChains: conf.GetNotificationChains(schedule.Conf, warnNotifications),
 			CritNotificationChains: conf.GetNotificationChains(schedule.Conf, critNotifications),
+		}
+		if parsedExpr == nil {
+			summaries = append(summaries, is)
+			continue
 		}
 		match, err := boolq.AskParsedExpr(*parsedExpr, is)
 		if err != nil {
