02d8a00e350401f61d36d454bef71c1a069b8b75
Kyle Brandt <kyle@kbrandt.com>
2016-07-20 10:31:37 -0400

    system.go comments

--- cmd/bosun/conf/system.go
+++ cmd/bosun/conf/system.go
@@ -95,7 +95,7 @@ type AnnotateConf struct {
 	Index string   // name of index / table
 }
 
-// LogstashConf contains a list of elastic hosts for the depcrecated logstash functions
+// LogStashConf contains a list of elastic hosts for the depcrecated logstash functions
 type LogStashConf struct {
 	Hosts expr.LogstashElasticHosts
 }
@@ -105,7 +105,7 @@ type ElasticConf struct {
 	Hosts expr.ElasticHosts
 }
 
-// ElasticConf contains configuration for an influx host that Bosun can query
+// InfluxConf contains configuration for an influx host that Bosun can query
 type InfluxConf struct {
 	URL       URL
 	Username  string
@@ -196,114 +196,158 @@ func LoadSystemConfig(conf string) (*SystemConf, error) {
 	return sc, nil
 }
 
+// GetHTTPListen returns the hostname:port that Bosun should listen on
 func (sc *SystemConf) GetHTTPListen() string {
 	return sc.HTTPListen
 }
 
+// GetRelayListen returns an address on which bosun will listen and Proxy all requests to /api
+// it was added so one can make OpenTSDB API endpoints available at the same URL as Bosun.
 func (sc *SystemConf) GetRelayListen() string {
 	return sc.RelayListen
 }
 
+// GetSMTPHost returns the SMTP mail server host that Bosun will use to relay through
 func (sc *SystemConf) GetSMTPHost() string {
 	return sc.SMTPConf.Host
 }
 
+// GetSMTPUsername returns the SMTP username that Bosun will use to connect to the mail server
 func (sc *SystemConf) GetSMTPUsername() string {
 	return sc.SMTPConf.Username
 }
 
+// GetSMTPPassword returns the SMTP password that Bosun will use to connect to the mail server
 func (sc *SystemConf) GetSMTPPassword() string {
 	return sc.SMTPConf.Password
 }
 
+// GetEmailFrom returns the email address that Bosun will use to send mail notifications from
 func (sc *SystemConf) GetEmailFrom() string {
 	return sc.SMTPConf.EmailFrom
 }
 
+// GetPing returns if Bosun's pinging is enabled. When Ping is enabled, bosun will ping all hosts
+// that is has indexed and record metrics about those pings.
 func (sc *SystemConf) GetPing() bool {
 	return sc.Ping
 }
 
+// GetPingDuration returns the duration that discovered hosts (will be pinged until
+// the host is not seen.
 func (sc *SystemConf) GetPingDuration() time.Duration {
 	return sc.PingDuration.Duration
 }
 
+// GetLedisDir returns the directory where Ledis should store its files
 func (sc *SystemConf) GetLedisDir() string {
 	return sc.DBConf.LedisDir
 }
 
+// GetLedisBindAddr returns the address that Ledis should listen on
 func (sc *SystemConf) GetLedisBindAddr() string {
 	return sc.DBConf.LedisBindAddr
 }
 
+// GetRedisHost returns the host to use for Redis. If this is set than Redis
+// will be used instead of Ledis.
 func (sc *SystemConf) GetRedisHost() string {
 	return sc.DBConf.RedisHost
 }
 
+// GetRedisDb returns the redis database number to use
 func (sc *SystemConf) GetRedisDb() int {
 	return sc.DBConf.RedisDb
 }
 
+// GetRedisPassword returns the password that should be used to connect to redis
 func (sc *SystemConf) GetRedisPassword() string {
 	return sc.DBConf.RedisPassword
 }
 
+// GetTimeAndDate returns the http://www.timeanddate.com/ that should be available to the UI
+// so it can show links to translate UTC times to various timezones. This feature is only
+// for creating UI Links as Bosun is expected to be running on a machine that is set to UTC
 func (sc *SystemConf) GetTimeAndDate() []int {
 	return sc.TimeAndDate
 }
 
+// GetSearchSince returns the duration that certain search requests should filter out results
+// if they are older (have not been indexed) since the duration
 func (sc *SystemConf) GetSearchSince() opentsdb.Duration {
 	return sc.SearchSince
 }
 
+// GetCheckFrequency returns the default CheckFrequency that the schedule should run at. Checks by
+// default will run at CheckFrequency * RunEvery
 func (sc *SystemConf) GetCheckFrequency() time.Duration {
 	return sc.CheckFrequency.Duration
 }
 
+// GetDefaultRunEvery returns the default multipler of how often an alert should run based on
+// the CheckFrequency. Checks by default will run at CheckFrequency * RunEvery
 func (sc *SystemConf) GetDefaultRunEvery() int {
 	return sc.DefaultRunEvery
 }
 
+// GetUnknownThreshold returns the threshold in which multiple unknown alerts in a check iteration
+// should be grouped into a single notification
 func (sc *SystemConf) GetUnknownThreshold() int {
 	return sc.UnknownThreshold
 }
 
+// GetMinGroupSize returns the minimum number of alerts needed to group the alerts
+// on Bosun's dashboard
 func (sc *SystemConf) GetMinGroupSize() int {
 	return sc.MinGroupSize
 }
 
+// GetShortURLKey returns the API key that should be used to generate https://goo.gl/ shortlinks
+// from Bosun's UI
 func (sc *SystemConf) GetShortURLKey() string {
 	return sc.ShortURLKey
 }
 
+// GetInternetProxy sets a proxy for outgoing network requests from Bosun. Currently it
+// only impacts requests made for shortlinks to https://goo.gl/
 func (sc *SystemConf) GetInternetProxy() string {
 	return sc.InternetProxy
 }
 
+// GetCommandHookPath returns the path of a command that should be run on every save
 func (sc *SystemConf) GetCommandHookPath() string {
 	return sc.CommandHookPath
 }
 
+// GetRuleFilePath returns the path to the file containing contains rules
+// rules include Alerts, Macros, Notifications, Templates, and Global Variables
 func (sc *SystemConf) GetRuleFilePath() string {
 	return sc.RuleFilePath
 }
 
+// SetTSDBHost sets the OpenTSDB host and used when Bosun is set to readonly mode
 func (sc *SystemConf) SetTSDBHost(tsdbHost string) {
 	sc.OpenTSDBConf.Host = tsdbHost
 }
 
+// GetTSDBHost returns the configured TSDBHost
 func (sc *SystemConf) GetTSDBHost() string {
 	return sc.OpenTSDBConf.Host
 }
 
+// GetLogstashElasticHosts returns the Hosts to connect to for issuing logstash
+// functions (which are depcrecated)
 func (sc *SystemConf) GetLogstashElasticHosts() expr.LogstashElasticHosts {
 	return sc.LogStashConf.Hosts
 }
 
+// GetAnnotateElasticHosts returns the Elastic hosts that should be used for annotations.
+// Annotations are not enabled if this has no hosts
 func (sc *SystemConf) GetAnnotateElasticHosts() expr.ElasticHosts {
 	return sc.AnnotateConf.Hosts
 }
 
+// GetAnnotateIndex returns the name of the Elastic index that should be used for annotations
 func (sc *SystemConf) GetAnnotateIndex() string {
 	return sc.AnnotateConf.Index
 }
@@ -317,8 +361,8 @@ func (sc *SystemConf) GetTSDBContext() opentsdb.Context {
 	return opentsdb.NewLimitContext(sc.OpenTSDBConf.Host, sc.OpenTSDBConf.ResponseLimit, sc.OpenTSDBConf.Version)
 }
 
-// GraphiteContext returns a Graphite context. A nil context is returned if
-// GraphiteHost is not set.
+// GetGraphiteContext returns a Graphite context which contains all the information needed
+// to query Graphite. A nil context is returned if GraphiteHost is not set.
 func (sc *SystemConf) GetGraphiteContext() graphite.Context {
 	if sc.GraphiteConf.Host == "" {
 		return nil
@@ -336,6 +380,8 @@ func (sc *SystemConf) GetGraphiteContext() graphite.Context {
 	return graphite.Host(sc.GraphiteConf.Host)
 }
 
+// GetInfluxContext returns a Influx context which contains all the information needed
+// to query Influx.
 func (sc *SystemConf) GetInfluxContext() client.Config {
 	c := client.NewConfig()
 	if sc.md.IsDefined("URL") {
@@ -359,18 +405,24 @@ func (sc *SystemConf) GetInfluxContext() client.Config {
 	return c
 }
 
+// GetLogstashContext returns a Logstash context which contains all the information needed
+// to query Elastic for logstash style queries. This is deprecated
 func (sc *SystemConf) GetLogstashContext() expr.LogstashElasticHosts {
 	return sc.LogStashConf.Hosts
 }
 
+// GetElasticContext returns an Elastic context which contains all the information
+// needed to run Elastic queries.
 func (sc *SystemConf) GetElasticContext() expr.ElasticHosts {
 	return sc.ElasticConf.Hosts
 }
 
+// AnnotateEnabled returns if annotations have been enabled or not
 func (sc *SystemConf) AnnotateEnabled() bool {
 	return len(sc.AnnotateConf.Hosts) != 0
 }
 
+// MakeLink creates a HTML Link based on Bosun's configured Hostname
 func (sc *SystemConf) MakeLink(path string, v *url.Values) string {
 	u := url.URL{
 		Scheme:   "http",
@@ -387,6 +439,7 @@ type Duration struct {
 	time.Duration
 }
 
+// UnmarshalText is the method called by TOML when decoding a value
 func (d *Duration) UnmarshalText(text []byte) error {
 	var err error
 	d.Duration, err = time.ParseDuration(string(text))
@@ -399,6 +452,7 @@ type URL struct {
 	*url.URL
 }
 
+// UnmarshalText is the method called by TOML when decoding a value
 func (u *URL) UnmarshalText(text []byte) error {
 	var err error
 	u.URL, err = url.Parse(string(bytes.Trim(text, `\"`)))
