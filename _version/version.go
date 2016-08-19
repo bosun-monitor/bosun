6502b9d5c9baeba6ab9d88089420e79a5c63b505
Greg Bray <Greg.Bray@Gmail.com>
2016-08-19 16:41:28 -0600

    Change version.go to 0.6.0 and add runtime date stamp if buildTime is missing

--- _version/version.go
+++ _version/version.go
@@ -4,14 +4,17 @@ package version // import "bosun.org/_version"
 
 import (
 	"fmt"
+	"os"
 	"time"
+
+	"github.com/kardianos/osext"
 )
 
 // These variables will be set at linking time for official builds.
 // build.go will set date and sha, but `go get` will set none of these.
 var (
 	// Version number for official releases Updated manually before each release.
-	Version = "0.5.0"
+	Version = "0.6.0-pre"
 
 	// Set to any non-empty value by official release script
 	OfficialBuild string
@@ -27,6 +30,14 @@ func GetVersionInfo(app string) string {
 	version := ShortVersion()
 	if buildTime, err := time.Parse("20060102150405", VersionDate); err == nil {
 		build = " built " + buildTime.Format(time.RFC3339)
+	} else {
+		currentFilePath, err := osext.Executable()
+		if err == nil {
+			info, err := os.Stat(currentFilePath)
+			if err == nil {
+				build = " last modified " + info.ModTime().Format(time.RFC3339)
+			}
+		}
 	}
 	if VersionSHA != "" {
 		sha = fmt.Sprintf(" (%s)", VersionSHA)
