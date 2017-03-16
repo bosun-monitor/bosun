d6f11ac4c2f36caf0c747e2e1ec8562a2b6ea2af
Kyle Brandt <kyle@kbrandt.com>
2017-03-16 16:29:38 -0400

    fix logic, remove bad invalid tests

--- cmd/bosun/conf/rule/rule_test.go
+++ cmd/bosun/conf/rule/rule_test.go
@@ -70,6 +70,7 @@ func TestInvalid(t *testing.T) {
 		"lookup-key-pairs-dup": `conf: lookup-key-pairs-dup:3:1: at <entry b=2,a=1 { }>: duplicate entry`,
 		"crit-warn-unmatching-tags": `conf: crit-warn-unmatching-tags:1:0: at <alert broken {\n	cri...>: crit tags (a,c) and warn tags (c) must be equal`,
 		"depends-no-overlap": `conf: depends-no-overlap:1:0: at <alert broken {\n	dep...>: Depends and crit/warn must share at least one tag.`,
+		"crit-notification-no-template": `conf: crit-notification-no-template:5:0: at <alert a {\n	crit = 1...>: critNotification specified, but no template`,
 	}
 	for fname, reason := range names {
 		path := filepath.Join("invalid", fname)
