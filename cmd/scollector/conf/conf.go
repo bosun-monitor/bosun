45956558835f60f03cc43a6c8fdc638103308cf2
georges chaudy <georges.chaudy@skyscanner.net>
2017-08-11 17:52:50 +0100

    Add support for metric prefix (#2089)
    * Add support for metric prefix
    
    * Rename Prefix to MetricPrefix. Add Prefix to AddTS method.
    
    * Add Prefix documentation

--- cmd/scollector/conf/conf.go
+++ cmd/scollector/conf/conf.go
@@ -63,6 +63,9 @@ type Conf struct {
 	// UseSWbemServicesClient specifies if the wmi package should use SWbemServices.
 	UseSWbemServicesClient bool
 
+	// MetricPrefix prepended to all metrics path
+	MetricPrefix string
+
 	HAProxy        []HAProxy
 	SNMP           []SNMP
 	MIBS           map[string]MIB
--- cmd/scollector/doc.go
+++ cmd/scollector/doc.go
@@ -121,6 +121,8 @@ matching on Linux.
 PProf (string): optional IP:Port binding to be used for debugging with pprof.
 Examples: localhost:6060 for loopback or :6060 for all IP addresses.
 
+MetricPrefix (string): optional Prefix prepended to all metrics path.
+
 Collector configuration keys
 
 Following are configurations for collectors that do not autodetect.
