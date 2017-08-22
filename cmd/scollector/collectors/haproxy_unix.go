76021d1fd28b3fee4fc3595abc99afbcecfb2c73
Gregory Eric Sanderson <gsanderson@jive.com>
2017-08-22 14:51:11 -0400

    cmd/scollector: haproxy collector compatible with 1.7
    The haproxy collector used the numerical positions of the columns
    for collecting statistics. This breaks when the order of the columns
    changes, which is the case for the CSV format in haproxy 1.7.
    
    The name of the columns provided in the CSV header is now used for
    fetching the right statistics.

--- cmd/scollector/collectors/haproxy_unix.go
+++ cmd/scollector/collectors/haproxy_unix.go
@@ -71,18 +71,31 @@ func haproxyFetch(user, pwd, tier, url string) (opentsdb.MultiDataPoint, error) 
 	if len(records[1]) < 16 {
 		return nil, fmt.Errorf("expected more columns with data. got: %v", len(records[1]))
 	}
-
+	header := parseHeader(records[0])
 	for _, rec := range records[1:] {
-		hType := haproxyType[rec[32]]
-		pxname := rec[0]
-		svname := rec[1]
+		typePos, ok := header["type"]
+		if !ok {
+			return nil, fmt.Errorf("type not found in haproxy header")
+		}
+		pxnamePos, ok := header["pxname"]
+		if !ok {
+			return nil, fmt.Errorf("pxname not found in haproxy header")
+		}
+		svnamePos, ok := header["svname"]
+		if !ok {
+			return nil, fmt.Errorf("svname not found in haproxy header")
+		}
+		hType := haproxyType[rec[typePos]]
+		pxname := rec[pxnamePos]
+		svname := rec[svnamePos]
 		ts := opentsdb.TagSet{"pxname": pxname, "svname": svname, "tier": tier}
-		for i, field := range haproxyCSVMeta {
-			if i >= len(rec) {
-				break
+		for _, field := range haproxyCSVMeta {
+			fieldPos, ok := header[field.Name]
+			if !ok {
+				return nil, fmt.Errorf("%s not found in haproxy header", field.Name)
 			}
 			m := strings.Join([]string{metric, hType, field.Name}, ".")
-			value := rec[i]
+			value := rec[fieldPos]
 			if field.Ignore == true {
 				continue
 			} else if strings.HasPrefix(field.Name, "hrsp") {
@@ -124,6 +137,20 @@ func haproxyFetch(user, pwd, tier, url string) (opentsdb.MultiDataPoint, error) 
 		}
 	}
 	return md, nil
+}
+
+// Convert the first line of the CSV data into a map
+// of column name -> column position.
+func parseHeader(fields []string) map[string]int {
+	header := map[string]int{}
+	for i, name := range fields {
+		if i == 0 {
+			header[strings.TrimLeft(name, " #")] = i
+		} else {
+			header[name] = i
+		}
+	}
+	return header
 }
 
 // MetricMetaHAProxy is a super-structure which adds a friendly Name,
