289979c700e02a365dcab9a1f318831e31e3ee1a
Mark Henderson <mhenderson@stackoverflow.com>
2017-08-31 10:02:20 -0400

    cmd/scollector: Updated Azure EA collector to add new tags. Converted to better invocation style. (#2011)

--- cmd/scollector/conf/conf.go
+++ cmd/scollector/conf/conf.go
@@ -164,9 +164,11 @@ type AWS struct {
 }
 
 type AzureEA struct {
-	EANumber          uint32
-	APIKey            string
-	LogBillingDetails bool
+	EANumber           uint32
+	APIKey             string
+	LogBillingDetails  bool
+	LogResourceDetails bool
+	LogExtraTags       bool
 }
 
 type SNMP struct {
