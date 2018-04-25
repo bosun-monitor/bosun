35d383d2cc24a9f42bd1f126a147cb6c29f98b1f
Kyle Brandt <kyle@kbrandt.com>
2018-04-25 13:20:27 -0400

    add metrics

--- cmd/scollector/collectors/ras_windows.go
+++ cmd/scollector/collectors/ras_windows.go
@@ -21,6 +21,7 @@ func init() {
 			fmt.Println(err)
 		}
 		// If RaMgmtSvc and has at least one entry - we monitor it
+		// TODO: Follow init / Enable, see if it is okay to scope `dst` this way
 		c.Enable = func() bool { return err == nil && len(dst) > 0 }
 	}
 	collectors = append(collectors, c)
@@ -41,7 +42,18 @@ func c_remote_access_services() (opentsdb.MultiDataPoint, error) {
 	totals := dst[0]
 	var md opentsdb.MultiDataPoint
 	tags := opentsdb.TagSet{}
-	Add(&md, rasPrefix+"total_connections", totals.TotalConnections, tags, metadata.Gauge, metadata.Connection, "TODO: DESC")
+	ts := TSys100NStoEpoch(totals.Timestamp_Sys100NS)
+	Add(&md, rasPrefix+"total_connections", totals.TotalConnections, tags, metadata.Gauge, metadata.Connection, descWinRasTotalConnections)
+	AddTS(&md, rasPrefix+"total_bytes", ts, totals.BytesReceived, opentsdb.TagSet{"direction": "in"}, metadata.Counter, metadata.Bytes, descWinRasTotalBytes)
+	AddTS(&md, rasPrefix+"total_bytes", ts, totals.BytesTransmitted, opentsdb.TagSet{"direction": "out"}, metadata.Counter, metadata.Bytes, descWinRasTotalBytes)
+	AddTS(&md, rasPrefix+"total_frames", ts, totals.FramesReceived, opentsdb.TagSet{"direction": "in"}, metadata.Counter, metadata.Frame, descWinRasFrames)
+	AddTS(&md, rasPrefix+"total_frames", ts, totals.FramesTransmitted, opentsdb.TagSet{"direction": "out"}, metadata.Counter, metadata.Frame, descWinRasFrames)
+	AddTS(&md, rasPrefix+"total_errors_by_type", ts, totals.AlignmentErrors, opentsdb.TagSet{"type": "alignment"}, metadata.Counter, metadata.Error, descWinRasErrorByType)
+	AddTS(&md, rasPrefix+"total_errors_by_type", ts, totals.BufferOverrunErrors, opentsdb.TagSet{"type": "buffer_overrun"}, metadata.Counter, metadata.Error, descWinRasErrorByType)
+	AddTS(&md, rasPrefix+"total_errors_by_type", ts, totals.CRCErrors, opentsdb.TagSet{"type": "crc"}, metadata.Counter, metadata.Error, descWinRasErrorByType)
+	AddTS(&md, rasPrefix+"total_errors_by_type", ts, totals.SerialOverrunErrors, opentsdb.TagSet{"type": "serial_overrun"}, metadata.Counter, metadata.Error, descWinRasErrorByType)
+	AddTS(&md, rasPrefix+"total_errors_by_type", ts, totals.TimeoutErrors, opentsdb.TagSet{"type": "timeout"}, metadata.Counter, metadata.Error, descWinRasErrorByType)
+	AddTS(&md, rasPrefix+"total_errors", ts, totals.TotalErrors, tags, metadata.Counter, metadata.Error, descWinRasTotalErrors)
 	return md, nil
 }
 
@@ -49,33 +61,30 @@ func c_remote_access_services() (opentsdb.MultiDataPoint, error) {
 // MSDN Reference https://msdn.microsoft.com/en-us/library/aa394330(v=vs.85).aspx
 // Only one or zero rows (instances) are expected in a query reply
 type Win32_PerfRawData_RemoteAccess_RASTotal struct {
-	AlignmentErrors         uint32
-	BufferOverrunErrors     uint32
-	BytesReceived           uint64
-	BytesReceivedPerSec     uint32
-	BytesTransmitted        uint64
-	BytesTransmittedPerSec  uint32
-	CRCErrors               uint32
-	FramesReceived          uint32
-	FramesReceivedPerSec    uint32
-	FramesTransmitted       uint32
-	FramesTransmittedPerSec uint32
-	Frequency_Object        uint64
-	Frequency_PerfTime      uint64
-	Frequency_Sys100NS      uint64
-	PercentCompressionIn    uint32
-	PercentCompressionOut   uint32
-	SerialOverrunErrors     uint32
-	TimeoutErrors           uint32
-	Timestamp_Object        uint64
-	Timestamp_PerfTime      uint64
-	Timestamp_Sys100NS      uint64
-	TotalConnections        uint32
-	TotalErrors             uint32
-	TotalErrorsPerSec       uint32
+	AlignmentErrors     uint32
+	BufferOverrunErrors uint32
+	BytesReceived       uint64
+	BytesTransmitted    uint64
+	CRCErrors           uint32
+	FramesReceived      uint32
+	FramesTransmitted   uint32
+	Frequency_Sys100NS  uint64
+	SerialOverrunErrors uint32
+	TimeoutErrors       uint32
+	Timestamp_Sys100NS  uint64
+	TotalConnections    uint32
+	TotalErrors         uint32
 }
 
-// Win32_PerfRawData_RamgmtSvcCounterProvider_RaMgmtSvc is used only to check for the existance of
+const (
+	descWinRasTotalConnections = "The total number of current Remote Access connections."
+	descWinRasTotalBytes       = "The total number of bytes transmitted and received for Remote Access connections."
+	descWinRasFrames           = "The total number of frames transmitted and received for Remote Access connections."
+	descWinRasErrorByType      = "The total number of errors on Remote Access connections by type: Alignment Errors occur when a byte received is different from the byte expected.  Buffer Overrun Errors when the software cannot handle the rate at which data is received. CRC Errors occur when the frame received contains erroneous data. Serial Overrun Errors occur when the hardware cannot handle the rate at which data is received. Timeout Errors occur when an expected is not received in time."
+	descWinRasTotalErrors      = "The total number of errors on Remote Access connections."
+)
+
+// Win32_PerfRawData_RamgmtSvcCounterProvider_RaMgmtSvc is used only to check for the existence of
 // an active ras service on the host to decide if we want to monitor it. We are not interested
 // in the actual values
 type Win32_PerfRawData_RamgmtSvcCounterProvider_RaMgmtSvc struct {
--- collect/collect.go
+++ collect/collect.go
@@ -39,7 +39,7 @@ var (
 
 	// DisableDefaultCollectors prevents the scollector self metrics from being
 	// generated.
-	DisableDefaultCollectors = true
+	DisableDefaultCollectors = false
 
 	// Tags is an opentsdb.TagSet used when sending self metrics.
 	Tags opentsdb.TagSet
