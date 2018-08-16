143eb6865196ac747b50d65547d5220dc58da887
Sizhe Yuen <sizhe1007@gmail.com>
2018-08-16 17:31:57 +0100

    Remove commented line

--- opentsdb/tsdb.go
+++ opentsdb/tsdb.go
@@ -944,7 +944,6 @@ func (r *Request) QueryResponse(host string, referer string, client *http.Client
 	req.Header.Set("Content-Type", "application/json")
 
 	resp, err := client.Do(req)
-	//resp, err := client.Post(u.String(), "application/json", bytes.NewReader(b))
 	if err != nil {
 		return nil, err
 	}
