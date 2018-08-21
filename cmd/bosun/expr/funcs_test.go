7d981cbe7b469c22a9816fee8b1fdb1525773edf
Herman Schaaf <herman.schaaf@skyscanner.net>
2018-08-21 16:16:32 +0100

    Fix change with upstream master in test

--- cmd/bosun/expr/funcs_test.go
+++ cmd/bosun/expr/funcs_test.go
@@ -7,8 +7,6 @@ import (
 	"time"
 
 	"bosun.org/opentsdb"
-
-	"github.com/influxdata/influxdb/client/v2"
 )
 
 type exprInOut struct {
@@ -482,7 +480,7 @@ func TestAggrNaNHandling(t *testing.T) {
 		InfluxConfig: client.HTTPConfig{},
 	}
 	providers := &BosunProviders{}
-	_, _, err = e.Execute(backends, providers, nil, queryTime, 0, false)
+	_, _, err = e.Execute(backends, providers, nil, queryTime, 0, false, nil)
 	if err != nil {
 		t.Fatal(err.Error())
 	}
