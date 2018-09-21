e561b29fa6bd64482c2aacd12363f43f2150291d
Danny Boland <email@dannyboland.com>
2018-09-21 17:49:12 +0100

    cmd/bosun: refactor tsdb query funcs to reuse bandTSDB()

--- cmd/bosun/expr/tsdb.go
+++ cmd/bosun/expr/tsdb.go
@@ -288,11 +288,6 @@ func windowCheck(t *parse.Tree, f *parse.FuncNode) error {
 	return nil
 }
 
-func Band(e *State, query, duration, period string, num float64) (r *Results, err error) {
-	// existing Band behaviour is to end 'period' ago, so pass period as eduration.
-	return BandQuery(e, query, duration, period, period, num)
-}
-
 func BandQuery(e *State, query, duration, period, eduration string, num float64) (r *Results, err error) {
 	r, err = bandTSDB(e, query, duration, period, eduration, num, func(r *Results, res *opentsdb.Response, offset time.Duration) error {
 		newarr := true
@@ -331,8 +326,8 @@ func BandQuery(e *State, query, duration, period, eduration string, num float64)
 	return
 }
 
-func ShiftBand(e *State, query, duration, period string, num float64) (r *Results, err error) {
-	r, err = bandTSDB(e, query, duration, period, period, num, func(r *Results, res *opentsdb.Response, offset time.Duration) error {
+func OverQuery(e *State, query, duration, period, eduration string, num float64) (r *Results, err error) {
+	r, err = bandTSDB(e, query, duration, period, eduration, num, func(r *Results, res *opentsdb.Response, offset time.Duration) error {
 		values := make(Series)
 		a := &Result{Group: res.Tags.Merge(opentsdb.TagSet{"shift": offset.String()})}
 		for k, v := range res.DPS {
@@ -352,80 +347,17 @@ func ShiftBand(e *State, query, duration, period string, num float64) (r *Result
 	return
 }
 
-func Over(e *State, query, duration, period string, num float64) (r *Results, err error) {
-	return OverQuery(e, query, duration, period, "", num)
+func Band(e *State, query, duration, period string, num float64) (r *Results, err error) {
+	// existing Band behaviour is to end 'period' ago, so pass period as eduration.
+	return BandQuery(e, query, duration, period, period, num)
 }
 
-func OverQuery(e *State, query, duration, period, eduration string, num float64) (r *Results, err error) {
-	r = new(Results)
-	r.IgnoreOtherUnjoined = true
-	r.IgnoreUnjoined = true
-	e.Timer.Step("band", func(T miniprofiler.Timer) {
-		var d, p opentsdb.Duration
-		d, err = opentsdb.ParseDuration(duration)
-		if err != nil {
-			return
-		}
-		p, err = opentsdb.ParseDuration(period)
-		if err != nil {
-			return
-		}
-		if num < 1 || num > 100 {
-			err = fmt.Errorf("num out of bounds")
-		}
-		var q *opentsdb.Query
-		q, err = opentsdb.ParseQuery(query, e.TSDBContext.Version())
-		if err != nil {
-			return
-		}
-		if !e.TSDBContext.Version().FilterSupport() {
-			if err = e.Search.Expand(q); err != nil {
-				return
-			}
-		}
-		req := opentsdb.Request{
-			Queries: []*opentsdb.Query{q},
-		}
-		end := e.now
-		if eduration != "" {
-			var ed opentsdb.Duration
-			ed, err = opentsdb.ParseDuration(eduration)
-			if err != nil {
-				return
-			}
-			end = end.Add(time.Duration(-ed))
-		}
-		req.End = end.Unix()
-		req.Start = end.Add(time.Duration(-d)).Unix()
-		for i := 0; i < int(num); i++ {
-			var s opentsdb.ResponseSet
-			s, err = timeTSDBRequest(e, &req)
-			if err != nil {
-				return
-			}
-			offset := e.now.Sub(end)
-			for _, res := range s {
-				if e.Squelched(res.Tags) {
-					continue
-				}
-				values := make(Series)
-				a := &Result{Group: res.Tags.Merge(opentsdb.TagSet{"shift": offset.String()})}
-				for k, v := range res.DPS {
-					i, err := strconv.ParseInt(k, 10, 64)
-					if err != nil {
-						return
-					}
-					values[time.Unix(i, 0).Add(offset).UTC()] = float64(v)
-				}
-				a.Value = values
-				r.Results = append(r.Results, a)
-			}
-			end = end.Add(time.Duration(-p))
-			req.End = end.Unix()
-			req.Start = end.Add(time.Duration(-d)).Unix()
-		}
-	})
-	return
+func ShiftBand(e *State, query, duration, period string, num float64) (r *Results, err error) {
+	return OverQuery(e, query, duration, period, period, num)
+}
+
+func Over(e *State, query, duration, period string, num float64) (r *Results, err error) {
+	return OverQuery(e, query, duration, period, "", num)
 }
 
 func Query(e *State, query, sduration, eduration string) (r *Results, err error) {
