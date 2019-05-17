e4cc36a5c66819b810ec46035d63818da6d5da23
Annette Wilson <annette.wilson@skyscanner.net>
2019-05-17 10:06:27 +0100

    opentsdb: support DropResets rate option (#2409)
    OpenTSDB query syntax allows `rate{dropcounter}` which acts like
    `rate{counter}` but additionally sets the rate option DropResets to
    true. Add support for this when parsing and serialising OpenTSDB
    queries.

--- opentsdb/tsdb.go
+++ opentsdb/tsdb.go
@@ -454,6 +454,7 @@ type RateOptions struct {
 	Counter    bool  `json:"counter,omitempty"`
 	CounterMax int64 `json:"counterMax,omitempty"`
 	ResetValue int64 `json:"resetValue,omitempty"`
+	DropResets bool  `json:"dropResets,omitempty"`
 }
 
 // ParseRequest parses OpenTSDB requests of the form: start=1h-ago&m=avg:cpu.
@@ -516,7 +517,8 @@ func ParseQuery(query string, version Version) (q *Query, err error) {
 			return
 		}
 		sp := strings.Split(s[1:len(s)-1], ",")
-		q.RateOptions.Counter = sp[0] == "counter"
+		q.RateOptions.Counter = sp[0] == "counter" || sp[0] == "dropcounter"
+		q.RateOptions.DropResets = sp[0] == "dropcounter"
 		if len(sp) > 1 {
 			if sp[1] != "" {
 				if q.RateOptions.CounterMax, err = strconv.ParseInt(sp[1], 10, 64); err != nil {
@@ -684,7 +686,12 @@ func (q Query) String() string {
 	if q.Rate {
 		s += "rate"
 		if q.RateOptions.Counter {
-			s += "{counter"
+			s += "{"
+			if q.RateOptions.DropResets {
+				s += "dropcounter"
+			} else {
+				s += "counter"
+			}
 			if q.RateOptions.CounterMax != 0 {
 				s += ","
 				s += strconv.FormatInt(q.RateOptions.CounterMax, 10)
