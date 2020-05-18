6d9f1ba884144fd8932d4927546f11fbec1815a0
Björn Marschollek <bjorn.marschollek@skyscanner.net>
2020-05-18 12:16:40 +0100

    Fix Windows build

--- annotate/cmd/annotate/os_windows.go
+++ annotate/cmd/annotate/os_windows.go
@@ -4,4 +4,4 @@ package main
 
 const timeFormat = "Monday, January 2, 2006 10:04:05 PM"
 const timeFormatUsage = "default returned by the powershell Get-Date commandlet. Assumed to be UTC"
-const USER_ENV = "USERNAME"
+const userEnvVar = "USERNAME"
